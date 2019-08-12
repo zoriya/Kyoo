5c3547fe61370d2fd1c891892f788e84eac17127
Zoe Roux <zoe.roux@sdg.moe>
2019-08-12 04:50:01 +0200

    Creating a basic navigation bar.

--- Kyoo/ClientApp/src/app/app.component.ts
+++ Kyoo/ClientApp/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent
 {
