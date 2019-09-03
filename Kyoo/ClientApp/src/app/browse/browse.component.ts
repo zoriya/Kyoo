754dd5fea1729a7765bc57968a76be48a02ae1b1
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2019-09-03 02:05:12 +0200

    Adding sort functions to the browser. Adding real tooltips.

--- Kyoo/ClientApp/src/app/browse/browse.component.ts
+++ Kyoo/ClientApp/src/app/browse/browse.component.ts
@@ -11,16 +11,46 @@ import { Show } from "../../models/show";
 export class BrowseComponent implements OnInit
 {
   shows: Show[];
+  sortType: string = "title";
+  sortUp: boolean = true;
+
+  sortTypes: string[] = ["title", "release date"];
 
   constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit()
   {
     this.shows = this.route.snapshot.data.shows;
+
+    $(document).ready(function ()
+    {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
   }
 
   getThumb(slug: string)
   {
     return this.sanitizer.bypassSecurityTrustStyle("url(/thumb/" + slug + ")");
+  }
+
+  sort(type: string, order: boolean)
+  {
+    this.sortType = type;
+    this.sortUp = order;
+
+    if (type == this.sortTypes[0])
+    {
+      if (order)
+        this.shows.sort((a, b) => { if (a.title < b.title) return -1; else if (a.title > b.title) return 1; return 0; });
+      else
+        this.shows.sort((a, b) => { if (a.title < b.title) return 1; else if (a.title > b.title) return -1; return 0; });
+    }
+    else if (type == this.sortTypes[1])
+    {
+      if (order)
+        this.shows.sort((a, b) => a.startYear - b.startYear);
+      else
+        this.shows.sort((a, b) => b.startYear - a.startYear);
+    }
   }
 }
