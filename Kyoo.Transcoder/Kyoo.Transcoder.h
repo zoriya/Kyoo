3096249eef4bd1c12ed309c4b6249956f28ea59e
Zoe Roux <zoe.roux@sdg.moe>
2019-09-04 23:02:00 +0200

    Reworking the way the c++ transcoder is imported.

--- Kyoo.Transcoder/Kyoo.Transcoder.h
+++
@@ -1,8 +0,0 @@
-﻿// Kyoo.Transcoder.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include "Transcoder.h"
-// TODO: Reference additional headers your program requires here.
--- Kyoo.Transcoder/Transcoder.cpp
+++ Kyoo.Transcoder/Transcoder.cpp
@@ -1,3 +1,6 @@
+#include "pch.h"
+#include "Transcoder.h"
+
 int Init()
 {
 	return 42;
