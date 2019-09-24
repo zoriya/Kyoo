3c47c88767ef135f132ab957a0a613ccb444f976
Zoe Roux <zoe.roux@sdg.moe>
2019-09-25 01:06:37 +0200

    Creating a Transmux function and cleaning up the Transcoder code.

--- Kyoo/InternalAPI/Transcoder/ITranscoder.cs
+++ Kyoo/InternalAPI/Transcoder/ITranscoder.cs
@@ -6,7 +6,7 @@ namespace Kyoo.InternalAPI
     public interface ITranscoder
     {
         //Should transcode to a mp4 container (same video/audio format if possible, no subtitles).
-        string Stream(string path);
+        string Transmux(WatchItem episode);
 
         //Should transcode to a mp4 container with a h264 video format and a AAC audio format, no subtitles.
         string Transcode(string path);
