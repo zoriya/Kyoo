c493ccafce42e0605dcf2845755452d5d291a8e3
Zoe Roux <zoe.roux@sdg.moe>
2019-09-27 01:25:25 +0200

    Making memory management more efficient.

--- Kyoo.Transcoder/src/Transcoder.h
+++ Kyoo.Transcoder/src/Transcoder.h
@@ -11,7 +11,9 @@
 
 extern "C" API int Init();
 
+extern "C" API int Transmux(const char *path, const char *outPath);
+
 //Take the path of the file and the path of the output directory. It will return the list of subtitle streams in the streams variable. The int returned is the number of subtitles extracted.
 extern "C" API Stream *ExtractSubtitles(const char *path, const char *outPath, int *streamCount, int *subtitleCount);
 
-extern "C" API int Transmux(const char *path, const char *outPath);
+extern "C" API void FreeMemory(Stream *streamsPtr);
