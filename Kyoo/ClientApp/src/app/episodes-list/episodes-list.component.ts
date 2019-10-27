06cbd21502e90dd78ae20f2ed0ed96bcfc61f41d
Zoe Roux <zoe.roux@sdg.moe>
2019-10-27 17:22:00 +0100

    Cleaning lists components

--- Kyoo/ClientApp/src/app/episodes-list/episodes-list.component.ts
+++ Kyoo/ClientApp/src/app/episodes-list/episodes-list.component.ts
@@ -1,59 +1,60 @@
-import { Component, OnInit, Input, SimpleChange } from '@angular/core';
-import { Episode } from "../../models/episode";
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { MatButton } from "@angular/material/button";
 import { DomSanitizer } from "@angular/platform-browser";
+import { Episode } from "../../models/episode";
 
 @Component({
   selector: 'app-episodes-list',
   templateUrl: './episodes-list.component.html',
   styleUrls: ['./episodes-list.component.scss']
 })
-export class EpisodesListComponent implements OnInit
+export class EpisodesListComponent
 {
 	@Input() displayShowTitle: boolean = false;
   @Input() episodes: Episode[];
-  private root: HTMLElement;
+	@ViewChild("scrollView", { static: true }) private scrollView: ElementRef;
+	@ViewChild("leftBtn", { static: false }) private leftBtn: MatButton;
+	@ViewChild("rightBtn", { static: false }) private rightBtn: MatButton;
+	@ViewChild("episode", { static: false }) private episode: ElementRef;
 
-  constructor(private sanitizer: DomSanitizer) { }
+	constructor(private sanitizer: DomSanitizer) { }
 
-  ngOnInit()
-  {
-    this.root = document.getElementById("episodes");
-  }
+	scrollLeft()
+	{
+		let scroll: number = this.roundScroll(this.scrollView.nativeElement.offsetWidth * 0.80);
+		this.scrollView.nativeElement.scrollBy({ top: 0, left: -scroll, behavior: "smooth" });
+	}
+
+	scrollRight()
+	{
+		let scroll: number = this.roundScroll(this.scrollView.nativeElement.offsetWidth * 0.80);
+		this.scrollView.nativeElement.scrollBy({ top: 0, left: scroll, behavior: "smooth" });
+	}
+
+	roundScroll(offset: number): number
+	{
+		let episodeSize: number = this.episode.nativeElement.scrollWidth;
+
+		offset = Math.round(offset / episodeSize) * episodeSize;
+		if (offset == 0)
+			offset = episodeSize;
+		return offset;
+	}
+
+	onScroll()
+	{
+		if (this.scrollView.nativeElement.scrollLeft <= 0)
+			this.leftBtn._elementRef.nativeElement.classList.add("d-none");
+		else
+			this.leftBtn._elementRef.nativeElement.classList.remove("d-none");
+		if (this.scrollView.nativeElement.scrollLeft >= this.scrollView.nativeElement.scrollWidth - this.scrollView.nativeElement.clientWidth)
+			this.rightBtn._elementRef.nativeElement.classList.add("d-none");
+		else
+			this.rightBtn._elementRef.nativeElement.classList.remove("d-none");
+	}
 
   sanitize(url: string)
   {
     return this.sanitizer.bypassSecurityTrustStyle("url(" + url + ")");
-  }
-
-  scrollLeft()
-  {
-    let scroll: number = this.roundScroll(this.root.offsetWidth * 0.80);
-    this.root.scrollBy({ top: 0, left: -scroll, behavior: "smooth" });
-
-    document.getElementById("el-rightBtn").classList.remove("d-none");
-
-    if (this.root.scrollLeft - scroll <= 0)
-      document.getElementById("el-leftBtn").classList.add("d-none");
-  }
-
-  scrollRight()
-  {
-    let scroll: number = this.roundScroll(this.root.offsetWidth * 0.80);
-    this.root.scrollBy({ top: 0, left: scroll, behavior: "smooth" });
-    document.getElementById("el-leftBtn").classList.remove("d-none");
-
-    if (this.root.scrollLeft + scroll >= this.root.scrollWidth - this.root.clientWidth)
-      document.getElementById("el-rightBtn").classList.add("d-none");
-  }
-
-  roundScroll(offset: number): number
-  {
-    let episodeSize: number = document.getElementById("el-1").scrollWidth;
-    offset = Math.round(offset / episodeSize) * episodeSize;
-
-    if (offset == 0)
-      offset = episodeSize;
-
-    return offset;
   }
 }
