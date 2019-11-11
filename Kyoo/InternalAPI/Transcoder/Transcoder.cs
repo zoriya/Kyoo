4fa5a6b30f8f804c0d02fbf0d28d2c59e37312e1
Zoe Roux <zoe.roux@sdg.moe>
2019-11-11 22:13:47 +0100

    The player now load transmuxed videos.

--- Kyoo/InternalAPI/Transcoder/Transcoder.cs
+++ Kyoo/InternalAPI/Transcoder/Transcoder.cs
@@ -11,11 +11,11 @@ namespace Kyoo.InternalAPI
 {
     public class Transcoder : ITranscoder
     {
-        private readonly string tempPath;
+        private readonly string transmuxPath;
 
         public Transcoder(IConfiguration config)
         {
-            tempPath = config.GetValue<string>("tempPath");
+            transmuxPath = config.GetValue<string>("transmuxTempPath");
 
             Debug.WriteLine("&Api INIT (unmanaged stream size): " + TranscoderAPI.Init() + ", Stream size: " + Marshal.SizeOf<Models.Watch.Stream>());
         }
@@ -33,7 +33,7 @@ namespace Kyoo.InternalAPI
 
         public string Transmux(WatchItem episode)
         {
-            string folder = Path.Combine(tempPath, episode.Link);
+            string folder = Path.Combine(transmuxPath, episode.Link);
             string manifest = Path.Combine(folder, episode.Link + ".mpd");
 
             Directory.CreateDirectory(folder);
@@ -48,7 +48,8 @@ namespace Kyoo.InternalAPI
 
         public string Transcode(string path)
         {
-            return @"D:\Videos\Anohana\AnoHana S01E01.mp4";
+            //NOT IMPLEMENTED YET
+            return null;
         }
     }
 }
