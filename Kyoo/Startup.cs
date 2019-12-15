e835e851d191976849a11c1f72896510ad4ee90a
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2019-12-15 23:14:49 +0100

    Moving out the stats GUI and checking for the error handler.

--- Kyoo/Startup.cs
+++ Kyoo/Startup.cs
@@ -78,9 +78,6 @@ namespace Kyoo
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
