126dff2fec9664b3deb88590fcb1edc05e9ee4ea
Zoe Roux <zoe.roux@sdg.moe>
2020-01-13 00:11:08 +0100

    Making a real good install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "The script must be run as root since it create an user for kyoo and install the app inside the /opt folder."
+    exit
+fi
+
 git clone https://github.com/AnonymusRaccoon/Kyoo --recurse
 cd Kyoo
 git pull --recurse
 chmod +x build.sh
 ./build.sh
 dotnet publish -c Release -o /opt/kyoo Kyoo/Kyoo.csproj
+useradd -rU kyoo
+chown -R kyoo /opt/kyoo
+chgrp -R kyoo /opt/kyoo
+chmod +x /opt/kyoo/kyoo.sh
