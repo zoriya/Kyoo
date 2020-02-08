e37d3f62990447d32684c29bb1060e6af3a682ce
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-02-08 17:10:59 +0100

    Auto creating the database on first start

--- Kyoo/Program.cs
+++ Kyoo/Program.cs
@@ -16,6 +16,10 @@ namespace Kyoo
             Console.WriteLine($"Running as: {Environment.UserName}");
             using (IServiceScope serviceScope = host.Services.CreateScope())
             {
+	            serviceScope.ServiceProvider.GetService<DatabaseContext>().Database.EnsureCreated();;
+	            // Use the next line if the database is not SQLite (SQLite doesn't support complexe migrations).
+	            // serviceScope.ServiceProvider.GetService<DatabaseContext>().Database.Migrate();;
+	            
                 IPluginManager pluginManager = serviceScope.ServiceProvider.GetService<IPluginManager>();
                 pluginManager.ReloadPlugins();
 
