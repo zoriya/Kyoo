562c39b9707fa5a9373c60527760065eb2a4670d
Zoe Roux <zoe.roux@sdg.moe>
2020-05-04 03:26:49 +0200

    Adding an identify for shows

--- Kyoo.Common/Controllers/IThumbnailsManager.cs
+++ Kyoo.Common/Controllers/IThumbnailsManager.cs
@@ -6,8 +6,8 @@ namespace Kyoo.Controllers
 {
 	public interface IThumbnailsManager
 	{
-		Task<Show> Validate(Show show);
-		Task<IEnumerable<PeopleLink>> Validate(IEnumerable<PeopleLink> actors);
-		Task<Episode> Validate(Episode episode);
+		Task<Show> Validate(Show show, bool alwaysDownload = false);
+		Task<IEnumerable<PeopleLink>> Validate(IEnumerable<PeopleLink> actors, bool alwaysDownload = false);
+		Task<Episode> Validate(Episode episode, bool alwaysDownload = false);
 	}
 }
