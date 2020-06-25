d704e46a93a0cab579282030a59217c3f06a3d27
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-06-25 03:21:29 +0200

    Disabling next/previous episode on movie's watchitems

--- Kyoo.Common/Models/WatchItem.cs
+++ Kyoo.Common/Models/WatchItem.cs
@@ -82,6 +82,9 @@ namespace Kyoo.Models
 				episode.Tracks.Where(x => x.Type == StreamType.Subtitle),
 				episode.Tracks.FirstOrDefault(x => x.Type == StreamType.Video))
 		{
+			if (episode.Show.IsMovie)
+				return;
+
 			if (EpisodeNumber > 1)
 				PreviousEpisode = episode.Season.Episodes.FirstOrDefault(x => x.EpisodeNumber == EpisodeNumber - 1);
 			else if (SeasonNumber > 1)
