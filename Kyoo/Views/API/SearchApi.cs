9aa87fffa840c697a9fdd1c6897ce8d93089044d
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-10-06 22:23:15 +0200

    Adding searchs for a specific ressource

--- Kyoo/Views/API/SearchApi.cs
+++ Kyoo/Views/API/SearchApi.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Kyoo.Controllers;
 using Kyoo.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -31,6 +32,53 @@ namespace Kyoo.Api
 				Genres = await _libraryManager.SearchGenres(query),
 				Studios = await _libraryManager.SearchStudios(query)
 			};
+		}
+		
+		[HttpGet("{query}/collection")]
+		[HttpGet("{query}/collections")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<Collection>> SearchCollections(string query)
+		{
+			return _libraryManager.SearchCollections(query);
+		}
+		
+		[HttpGet("{query}/show")]
+		[HttpGet("{query}/shows")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<Show>> SearchShows(string query)
+		{
+			return _libraryManager.SearchShows(query);
+		}
+		
+		[HttpGet("{query}/episode")]
+		[HttpGet("{query}/episodes")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<Episode>> SearchEpisodes(string query)
+		{
+			return _libraryManager.SearchEpisodes(query);
+		}
+		
+		[HttpGet("{query}/people")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<People>> SearchPeople(string query)
+		{
+			return _libraryManager.SearchPeople(query);
+		}
+		
+		[HttpGet("{query}/genre")]
+		[HttpGet("{query}/genres")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<Genre>> SearchGenres(string query)
+		{
+			return _libraryManager.SearchGenres(query);
+		}
+		
+		[HttpGet("{query}/studio")]
+		[HttpGet("{query}/studios")]
+		[Authorize(Policy="Read")]
+		public Task<ICollection<Studio>> SearchStudios(string query)
+		{
+			return _libraryManager.SearchStudios(query);
 		}
 	}
 }
