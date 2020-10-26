81e2f2bbb1796b2a480fe92587bb8fc0014facbd
Zoe Roux <zoe.roux@sdg.moe>
2020-10-26 03:07:29 +0100

    Adding comments to the post install sh

--- subtitles-octopus-fix.sh
+++ subtitles-octopus-fix.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+#
+# Small fix to support strict mode in subtitles-octopus while waiting for the PR to be merged & a new version to be released.
+#
+
+
 LINE_204="                self.video.addEventListener(\\\"loadedmetadata\\\", function listener(e) {"
 LINE_205="                    e.target.removeEventListener(e.type, listener);"
 
