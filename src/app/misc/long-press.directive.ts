50fda7a20530f58d7566ef7efaddd5f1fe79341d
Zoe Roux <zoe.roux@sdg.moe>
2021-01-10 21:11:03 +0100

    Fixing the long press directive

--- src/app/misc/long-press.directive.ts
+++ src/app/misc/long-press.directive.ts
@@ -1,4 +1,8 @@
 import { Directive, Output, EventEmitter, HostListener, HostBinding } from "@angular/core";
+import MouseDownEvent = JQuery.MouseDownEvent;
+import TouchStartEvent = JQuery.TouchStartEvent;
+import MouseMoveEvent = JQuery.MouseMoveEvent;
+import TouchMoveEvent = JQuery.TouchMoveEvent;
 
 @Directive({
 	selector: `[appLongPress]`
@@ -6,29 +10,39 @@ import { Directive, Output, EventEmitter, HostListener, HostBinding } from "@ang
 export class LongPressDirective
 {
 	@Output() longPressed = new EventEmitter();
-	timer: NodeJS.Timeout = null;
+	private _timer: NodeJS.Timeout = null;
 
 	@HostBinding('class.longpress')
 	get longPress(): boolean
 	{
-		return this.timer !== null;
+		return this._timer !== null;
 	}
 
 	@HostListener("touchstart", ["$event"])
 	@HostListener("mousedown", ["$event"])
-	start(): void
+	start(event: MouseDownEvent | TouchStartEvent): void
 	{
-		this.timer = setTimeout(() => {
+		const startBox = event.target.getBoundingClientRect();
+		this._timer = setTimeout(() =>
+		{
+			const endBox = event.target.getBoundingClientRect();
+			if (startBox.top !== endBox.top || startBox.left !== endBox.left)
+				return;
 			this.longPressed.emit();
+			this._timer = null;
 		}, 500);
 	}
 
 	@HostListener("touchend", ["$event"])
 	@HostListener("mouseup", ["$event"])
+	@HostListener("wheel", ["$event"])
+	@HostListener("scroll", ["$event"])
+	@HostListener("document.scroll", ["$event"])
+	@HostListener("window.scroll", ["$event"])
 	end(): void
 	{
-		clearTimeout(this.timer);
-		this.timer = null;
+		clearTimeout(this._timer);
+		this._timer = null;
 	}
 
 	@HostListener("contextmenu", ["$event"])
@@ -39,5 +53,4 @@ export class LongPressDirective
 
 	@HostBinding("style.-webkit-touch-callout")
 	defaultLongTouchEvent: string = "none";
-
 }
