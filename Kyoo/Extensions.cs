c8d72e1a876cd0c4c6bab03cabf0162fd621a656
Zoe Roux <zoe.roux@sdg.moe>
2021-03-29 00:40:11 +0200

    Creating a doxyfile

--- Kyoo/Extensions.cs
+++ Kyoo/Extensions.cs
@@ -3,8 +3,16 @@ using Microsoft.Extensions.Configuration;
 
 namespace Kyoo
 {
+	/// <summary>
+	/// A class that regroup extensions used by some asp-net related parts of the app.
+	/// </summary>
 	public static class Extensions
 	{
+		/// <summary>
+		/// Get a connection string from the Configuration's section "Databse"
+		/// </summary>
+		/// <param name="config">The IConfiguration instance to load.</param>
+		/// <returns>A parsed connection string</returns>
 		public static string GetDatabaseConnection(this IConfiguration config)
 		{
 			DbConnectionStringBuilder builder = new();
