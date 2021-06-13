1bb29be1343142a3478cb6aabf36d690b774be94
Zoe Roux <zoe.roux@sdg.moe>
2021-06-13 19:13:05 +0200

    Adding slugs tests for seasons

--- Kyoo.Tests/Library/SpecificTests/SeasonTests.cs
+++ Kyoo.Tests/Library/SpecificTests/SeasonTests.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using Kyoo.Controllers;
 using Kyoo.Models;
+using Xunit;
 
 namespace Kyoo.Tests.SpecificTests
 {
@@ -10,6 +12,35 @@ namespace Kyoo.Tests.SpecificTests
 		public SeasonTests()
 		{
 			_repository = Repositories.LibraryManager.SeasonRepository;
+		}
+
+		[Fact]
+		public async Task SlugEditTest()
+		{
+			Season season = await _repository.Get(1);
+			Assert.Equal("anohana-s1", season.Slug);
+			Show show = new()
+			{
+				ID = season.ShowID,
+				Slug = "new-slug"
+			};
+			await Repositories.LibraryManager.ShowRepository.Edit(show, false);
+			season = await _repository.Get(1);
+			Assert.Equal("new-slug-s1", season.Slug);
+		}
+		
+		[Fact]
+		public async Task SeasonNumberEditTest()
+		{
+			Season season = await _repository.Get(1);
+			Assert.Equal("anohana-s1", season.Slug);
+			season = await _repository.Edit(new Season
+			{
+				ID = 1,
+				SeasonNumber = 2
+			}, false);
+			season = await _repository.Get(1);
+			Assert.Equal("anohana-s2", season.Slug);
 		}
 	}
 }
