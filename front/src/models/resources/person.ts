608bc15e1f0a717f92f6fbfd243e0cd8a3b4aedb
Zoe Roux <zoe.roux@sdg.moe>
2022-09-06 03:26:06 +0200

    Add staff list on show page

--- front/src/models/resources/person.ts
+++ front/src/models/resources/person.ts
@@ -18,11 +18,29 @@
  * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
  */
 
-export * from "./library";
-export * from "./library-item";
-export * from "./show";
-export * from "./movie";
-export * from "./collection";
-export * from "./genre";
-export * from "./person";
-export * from "./studio";
+import { z } from "zod";
+import { ImagesP } from "../traits";
+import { ResourceP } from "../traits/resource";
+
+export const PersonP = ResourceP.merge(ImagesP).extend({
+	/**
+	 * The name of this person.
+	 */
+	name: z.string(),
+	/**
+	 * The type of work the person has done for the show. That can be something like "Actor",
+	 * "Writer", "Music", "Voice Actor"...
+	 */
+	type: z.string().optional(),
+
+	/**
+	 * The role the People played. This is mostly used to inform witch character was played for actor
+	 * and voice actors.
+	 */
+	role: z.string().optional(),
+});
+
+/**
+ * A studio that make shows.
+ */
+export type Person = z.infer<typeof PersonP>;
