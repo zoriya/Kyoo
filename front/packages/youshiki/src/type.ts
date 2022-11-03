2c4bcb9c39af0a8f45cec9dc9be4f789b4841170
Zoe Roux <zoe.roux@sdg.moe>
2022-11-04 02:45:34 +0900

    Create CssObject type

---
+++ front/packages/youshiki/src/type.ts
@@ -0,0 +1,36 @@
+/*
+ * Kyoo - A portable and vast media library solution.
+ * Copyright (c) Kyoo.
+ *
+ * See AUTHORS.md and LICENSE file in the project root for full license information.
+ *
+ * Kyoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Kyoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export type Theme = {
+	// TODO: remove this, just for test purpose
+	spacing: string;
+};
+
+export type YoushikiStyle<Property> =
+	| Property
+	| ((theme: Theme) => Property)
+	| Breakpoints<Property>;
+
+export type Breakpoints<Property> = {
+	sm?: Property;
+	md?: Property;
+	lg?: Property;
+	xl?: Property;
+};
