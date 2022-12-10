a486626969ce3f7fd3edf30ea9491adca0f3a30e
Zoe Roux <zoe.roux@sdg.moe>
2022-12-10 14:41:03 +0900

    Add an infinite scroll on web and native

---
+++ front/packages/ui/src/fetch-infinite.tsx
@@ -0,0 +1,78 @@
+/*
+ * Kyoo - A portable and vast media library solution.
+ * Copyright (c) Kyoo.
+ *
+ * See AUTHORS.md and LICENSE file in the project root for full license information.
+ *
+ * Kyoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Kyoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Page, QueryIdentifier, useInfiniteFetch } from "@kyoo/models";
+import { FlashList } from "@shopify/flash-list";
+import { ReactElement } from "react";
+import { ErrorView, WithLoading } from "./fetch";
+
+export const InfiniteFetch = <Data,>({
+	query,
+	placeholderCount = 15,
+	children,
+	size,
+	numColumns,
+	...props
+}: {
+	query: QueryIdentifier<Data>;
+	placeholderCount?: number;
+	numColumns: number;
+	children: (
+		item: Data extends Page<infer Item> ? WithLoading<Item> : WithLoading<Data>,
+		key: string | undefined,
+		i: number,
+	) => ReactElement | null;
+		size: number;
+}): JSX.Element | null => {
+	if (!query.infinite) console.warn("A non infinite query was passed to an InfiniteFetch.");
+
+	const { items, error, fetchNextPage, hasNextPage, refetch, isRefetching } =
+		useInfiniteFetch(query);
+
+	if (error) return <ErrorView error={error} />;
+
+	return (
+		<FlashList
+			renderItem={({ item, index }) =>
+				children({ isLoading: false, ...item } as any, undefined, index)
+			}
+			data={
+				hasNextPage
+					? [
+							...(items || []),
+							...[
+								...Array(
+									items ? numColumns - (items.length % numColumns) + numColumns : placeholderCount,
+								),
+							].map((_, i) => ({ id: `gen${i}`, isLoading: true } as Data)),
+					  ]
+					: items
+			}
+			keyExtractor={(item: any) => item.id?.toString()}
+			numColumns={numColumns}
+			estimatedItemSize={size}
+			onEndReached={fetchNextPage}
+			onEndReachedThreshold={0.5}
+			onRefresh={refetch}
+			refreshing={isRefetching}
+			{...props}
+		/>
+	);
+};
---
+++ front/packages/ui/src/fetch-infinite.web.tsx
@@ -0,0 +1,69 @@
+/*
+ * Kyoo - A portable and vast media library solution.
+ * Copyright (c) Kyoo.
+ *
+ * See AUTHORS.md and LICENSE file in the project root for full license information.
+ *
+ * Kyoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Kyoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Page, QueryIdentifier, useInfiniteFetch } from "@kyoo/models";
+import { ReactElement } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useYoshiki } from "yoshiki";
+import { ErrorView, WithLoading } from "./fetch";
+
+export const InfiniteFetch = <Data,>({
+	query,
+	placeholderCount = 15,
+	children,
+	...props
+}: {
+	query: QueryIdentifier<Data>;
+	placeholderCount?: number;
+	children: (
+		item: Data extends Page<infer Item> ? WithLoading<Item> : WithLoading<Data>,
+		key: string | undefined,
+		i: number,
+	) => ReactElement | null;
+}): JSX.Element | null => {
+	if (!query.infinite) console.warn("A non infinite query was passed to an InfiniteFetch.");
+
+	const { items, error, fetchNextPage, hasNextPage } = useInfiniteFetch(query);
+	const { css } = useYoshiki();
+
+	if (error) return <ErrorView error={error} />;
+
+	return (
+		<InfiniteScroll
+			dataLength={items?.length ?? 0}
+			next={fetchNextPage}
+			hasMore={hasNextPage!}
+			loader={[...Array(12)].map((_, i) => children({ isLoading: true } as any, i.toString(), i))}
+			{...css(
+				{
+					display: "flex",
+					flexWrap: "wrap",
+					alignItems: "flex-start",
+					justifyContent: "center",
+				},
+				props,
+			)}
+		>
+			{items?.map((item, i) =>
+				children({ ...item, isLoading: false } as any, (item as any).id?.toString(), i),
+			)}
+		</InfiniteScroll>
+	);
+};
