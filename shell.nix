7427de1bb429f15065c24d3c88b43d0b563451b2
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-17 12:58:58 +0900

    Add guessit

--- shell.nix
+++ shell.nix
@@ -1,13 +1,18 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  packages = with pkgs; [
-    nodejs-16_x
-    nodePackages.yarn
-    (with dotnetCorePackages;
-      combinePackages [
-        sdk_6_0
-        aspnetcore_6_0
-      ])
-	python3
-  ];
-}
+{pkgs ? import <nixpkgs> {}}: let
+  pythonPackages = p:
+    with p; [
+      guessit
+    ];
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      nodejs-16_x
+      nodePackages.yarn
+      (with dotnetCorePackages;
+        combinePackages [
+          sdk_6_0
+          aspnetcore_6_0
+        ])
+      (python3.withPackages pythonPackages)
+    ];
+  }
