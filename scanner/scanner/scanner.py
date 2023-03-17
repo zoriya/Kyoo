c4423ed7a1d7923acf3e8d169ed9319202acdeab
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-17 12:58:58 +0900

    Add guessit

--- scanner/scanner/scanner.py
+++ scanner/scanner/scanner.py
@@ -1,7 +1,14 @@
 from pathlib import Path
+from guessit import guessit
+import logging
 
 def scan(path: str):
 	for item in Path(path).rglob("*"):
 		if not item.is_file():
 			continue
-		print(item)
+		identify(item)
+
+def identify(path: Path):
+	raw = guessit(path)
+	logging.info("Identied %s: %s", path, raw)
+	# print(f'type: {raw["type"]}, title: {raw["title"]}')
