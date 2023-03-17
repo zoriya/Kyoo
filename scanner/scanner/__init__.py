0ca6575f42ca1358cad60410217c165d3edfde67
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-17 12:58:58 +0900

    Add guessit

--- scanner/scanner/__init__.py
+++ scanner/scanner/__init__.py
@@ -2,8 +2,13 @@ from .scanner import scan
 
 def main():
 	import os
-	path = os.environ.get("LIBRARY_PATH")
+	import logging
+	import sys
+
+	path = os.environ.get("LIBRARY_ROOT")
 	if not path:
-		print("Missing environment variable 'LIBRARY_PATH'.")
+		print("Missing environment variable 'LIBRARY_ROOT'.")
 		exit(2)
+	if len(sys.argv) > 1 and sys.argv[1] == "-v":
+		logging.basicConfig(level=logging.INFO)
 	return scan(path)
