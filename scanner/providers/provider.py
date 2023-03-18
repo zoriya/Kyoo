bfd1d20006a503e9147b489a8ad9abc7666f0f14
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-18 23:37:38 +0900

    Add black to lint the scannre

--- scanner/providers/provider.py
+++ scanner/providers/provider.py
@@ -8,18 +8,21 @@ from .types.movie import Movie
 
 Self = TypeVar("Self", bound="Provider")
 
+
 class Provider:
 	@classmethod
 	def get_all(cls: type[Self], client: ClientSession) -> list[Self]:
+		from providers.implementations.themoviedatabase import TheMovieDatabase
 		providers = []
 
-		from providers.implementations.themoviedatabase import TheMovieDatabase
-		tmdb =os.environ.get("THEMOVIEDB_APIKEY")
+		tmdb = os.environ.get("THEMOVIEDB_APIKEY")
 		if tmdb:
 			providers.append(TheMovieDatabase(client, tmdb))
 
 		return providers
 
 	@abstractmethod
-	async def identify_movie(self, name: str, year: Optional[int], *, language: list[str]) -> Movie:
+	async def identify_movie(
+		self, name: str, year: Optional[int], *, language: list[str]
+	) -> Movie:
 		raise NotImplementedError
