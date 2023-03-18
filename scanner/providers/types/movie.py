fcfbe5bbf07dd528e28ca5bc5bd1ff6ffa3e28c3
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-18 17:42:55 +0900

    Create python types

---
+++ scanner/providers/types/movie.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+from .genre import Genre
+from .status import Status
+
+@dataclass
+class MovieTranslation:
+	name: str
+	keywords: list[str] = field(default_factory=list)
+	overview: Optional[str] = None
+
+@dataclass
+class Movie:
+	aliases: list[str] = field(default_factory=list)
+	release_date: Optional[datetime | int] = None
+	status: Status = Status.UNKNOWN
+	studio: Optional[int | str] = None
+	genres: list[Genre] = field(default_factory=list)
+
+	poster: list[str] = field(default_factory=list)
+	thumbnails: list[str] = field(default_factory=list)
+	logo: list[str] = field(default_factory=list)
+
+	path: Optional[str] = None
+	# TODO: handle staff
+	# staff: list[Staff]
+
+	translations: dict[str, MovieTranslation] = field(default_factory=dict)
+
