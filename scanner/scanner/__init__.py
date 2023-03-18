80568db392ca730e75d8b425b2fb12496600cad2
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-18 17:42:37 +0900

    Make the scanner async

--- scanner/scanner/__init__.py
+++ scanner/scanner/__init__.py
@@ -1,14 +1,20 @@
-from .scanner import scan
+from .scanner import Scanner
 
-def main():
+async def main():
 	import os
 	import logging
 	import sys
+	from aiohttp import ClientSession
 
 	path = os.environ.get("LIBRARY_ROOT")
 	if not path:
 		print("Missing environment variable 'LIBRARY_ROOT'.")
 		exit(2)
+	languages = os.environ.get("LIBRARY_LANGUAGES")
+	if not languages:
+		print("Missing environment variable 'LIBRARY_LANGUAGES'.")
+		exit(2)
 	if len(sys.argv) > 1 and sys.argv[1] == "-v":
-		logging.basicConfig(level=logging.INFO)
-	return scan(path)
+		logging.basicConfig(level=logging.DEBUG)
+	async with ClientSession() as client:
+		await Scanner(client, languages.split(',')).scan(path)
