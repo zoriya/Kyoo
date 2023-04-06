076222605df7f6a8b6ea9ae078e8f92603d35863
Zoe Roux <zoe.roux@zoriya.dev>
2023-04-06 16:24:37 +0900

    Migrate to actix

--- transcoder/src/main.rs
+++ transcoder/src/main.rs
@@ -1,12 +1,20 @@
-#[macro_use]
-extern crate rocket;
+use actix_files::NamedFile;
+use actix_web::{get, web, App, HttpServer, Result};
+mod paths;
 
-#[get("/")]
-fn index() -> &'static str {
-	"Hello, world!"
+#[get("/movie/direct/{slug}")]
+async fn index(query: web::Path<String>) -> Result<NamedFile> {
+	let slug = query.into_inner();
+	let path = paths::get_movie_path(slug);
+
+	Ok(NamedFile::open_async(path).await?)
+		// .map(|f| (infer_content_type(f), f))
 }
 
-#[launch]
-fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+	HttpServer::new(|| App::new().service(index))
+		.bind(("0.0.0.0", 7666))?
+		.run()
+		.await
 }
---
+++ transcoder/src/paths.rs
@@ -0,0 +1,4 @@
+pub fn get_movie_path(_slug: String) -> String {
+	// TODO: Implement this method to fetch the path from the API.
+	String::from("/video/test.mkv")
+}
