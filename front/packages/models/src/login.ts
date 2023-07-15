96d5ca0f3c1c93b66eea4f85aaaba0b2ccc6d32c
Zoe Roux <zoe.roux@zoriya.dev>
2023-07-15 19:37:04 +0900

    Fix web build

--- front/packages/models/src/login.ts
+++ front/packages/models/src/login.ts
@@ -19,13 +19,11 @@
  */
 
 import { z } from "zod";
-import { deleteSecureItem, getSecureItem, setSecureItem, storage } from "./secure-store";
+import { deleteSecureItem, getSecureItem, setSecureItem } from "./secure-store";
 import { zdate } from "./utils";
-import { queryFn, setApiUrl } from "./query";
+import { queryFn } from "./query";
 import { KyooErrors } from "./kyoo-errors";
 import { Platform } from "react-native";
-import { createContext, useEffect, useState } from "react";
-import { useMMKVListener } from "react-native-mmkv";
 
 const TokenP = z.object({
 	token_type: z.literal("Bearer"),
@@ -41,65 +39,6 @@ type Result<A, B> =
 	| { ok: false; value?: undefined; error: B };
 
 export type Account = Token & { apiUrl: string; username: string };
-
-export const AccountContext = createContext<ReturnType<typeof useAccounts>>({ type: "loading" });
-
-export const useAccounts = () => {
-	const [accounts, setAccounts] = useState<Account[]>(JSON.parse(getSecureItem("accounts") ?? "[]"));
-	const [verified, setVerified] = useState<{
-		status: "ok" | "error" | "loading" | "unverified";
-		error?: string;
-	}>({ status: "loading" });
-	const [retryCount, setRetryCount] = useState(0);
-
-	const sel = getSecureItem("selected");
-	let [selected, setSelected] = useState<number | null>(
-		sel ? parseInt(sel) : accounts.length > 0 ? 0 : null,
-	);
-	if (selected === null && accounts.length > 0) selected = 0;
-	if (accounts.length === 0) selected = null;
-
-	useEffect(() => {
-		async function check() {
-			setVerified({status: "loading"});
-			const selAcc = accounts![selected!];
-			setApiUrl(selAcc.apiUrl);
-			const verif = await loginFunc("refresh", selAcc.refresh_token);
-			setVerified(verif.ok ? { status: "ok" } : { status: "error", error: verif.error });
-		}
-
-		if (accounts.length && selected !== null) check();
-		else setVerified({ status: "unverified" });
-	// Use the length of the array and not the array directly because we don't care if the refresh token changes.
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [accounts.length, selected, retryCount]);
-
-	useMMKVListener((key) => {
-		if (key === "accounts") setAccounts(JSON.parse(getSecureItem("accounts") ?? "[]"));
-	}, storage);
-
-	if (verified.status === "loading") return { type: "loading" } as const;
-	if (accounts.length && verified.status === "unverified") return { type: "loading" } as const;
-	if (verified.status === "error") {
-		return {
-			type: "error",
-			error: verified.error,
-			retry: () => {
-				setVerified({ status: "loading" });
-				setRetryCount((x) => x + 1);
-			},
-		} as const;
-	}
-	return {
-		type: "ok",
-		accounts,
-		selected,
-		setSelected: (selected: number) => {
-			setSelected(selected);
-			setSecureItem("selected", selected.toString());
-		},
-	} as const;
-};
 
 const addAccount = (token: Token, apiUrl: string, username: string | null) => {
 	const accounts: Account[] = JSON.parse(getSecureItem("accounts") ?? "[]");
