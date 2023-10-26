ce4663d512624c7e21c35c7d91c16f1f23ad8dd6
Zoe Roux <zoe.roux@zoriya.dev>
2023-10-27 00:03:54 +0200

    Fix some aria items

--- front/apps/web/src/pages/_app.tsx
+++ front/apps/web/src/pages/_app.tsx
@@ -120,6 +120,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 			<>
 				<Head>
 					<title>Kyoo</title>
+					<meta name="description" content="A portable and vast media library solution." />
 				</Head>
 				<QueryClientProvider client={queryClient}>
 					<Hydrate state={queryState}>
