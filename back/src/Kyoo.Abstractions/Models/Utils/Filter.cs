edc6d11824f7a152f93e99dda5f7a6fe75c7a696
Zoe Roux <zoe.roux@zoriya.dev>
2023-11-24 00:17:52 +0100

    Parse new filters

--- back/src/Kyoo.Abstractions/Models/Utils/Filter.cs
+++ back/src/Kyoo.Abstractions/Models/Utils/Filter.cs
@@ -17,8 +17,13 @@
 // along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using Sprache;
+
 namespace Kyoo.Abstractions.Models.Utils;
 
 public abstract record Filter
@@ -46,24 +51,154 @@ public abstract record Filter<T> : Filter
 
 	public record Eq(string property, object value) : Filter<T>;
 
-	public record Ne<T2>(string property, T2 value) : Filter<T>;
+	public record Ne(string property, object value) : Filter<T>;
 
-	public record Gt<T2>(string property, T2 value) : Filter<T>;
+	public record Gt(string property, object value) : Filter<T>;
 
-	public record Ge<T2>(string property, T2 value) : Filter<T>;
+	public record Ge(string property, object value) : Filter<T>;
 
-	public record Lt<T2>(string property, T2 value) : Filter<T>;
+	public record Lt(string property, object value) : Filter<T>;
 
-	public record Le<T2>(string property, T2 value) : Filter<T>;
+	public record Le(string property, object value) : Filter<T>;
 
-	public record Has<T2>(string property, T2 value) : Filter<T>;
+	public record Has(string property, object value) : Filter<T>;
 
 	public record In(string property, object[] value) : Filter<T>;
 
 	public record Lambda(Expression<Func<T, bool>> lambda) : Filter<T>;
 
-	public static Filter<T> From(string filter)
+	public static class FilterParsers
 	{
+		public static readonly Parser<Filter<T>> Filter =
+			Parse.Ref(() => Bracket)
+				.Or(Parse.Ref(() => Not))
+				.Or(Parse.Ref(() => Eq))
+				.Or(Parse.Ref(() => Ne))
+				.Or(Parse.Ref(() => Gt))
+				.Or(Parse.Ref(() => Ge))
+				.Or(Parse.Ref(() => Lt))
+				.Or(Parse.Ref(() => Le));
 
+		public static readonly Parser<Filter<T>> CompleteFilter =
+			Parse.Ref(() => Or)
+				.Or(Parse.Ref(() => And))
+				.Or(Filter);
+
+		public static readonly Parser<Filter<T>> Bracket =
+			from open in Parse.Char('(').Token()
+			from filter in CompleteFilter
+			from close in Parse.Char(')').Token()
+			select filter;
+
+		public static readonly Parser<IEnumerable<char>> AndOperator = Parse.IgnoreCase("and")
+			.Or(Parse.String("&&"))
+			.Token();
+
+		public static readonly Parser<IEnumerable<char>> OrOperator = Parse.IgnoreCase("or")
+			.Or(Parse.String("||"))
+			.Token();
+
+		public static readonly Parser<Filter<T>> And = Parse.ChainOperator(AndOperator, Filter, (_, a, b) => new Filter<T>.And(a, b));
+
+		public static readonly Parser<Filter<T>> Or = Parse.ChainOperator(OrOperator, And.Or(Filter), (_, a, b) => new Filter<T>.Or(a, b));
+
+		public static readonly Parser<Filter<T>> Not =
+			from not in Parse.IgnoreCase("not")
+				.Or(Parse.String("!"))
+				.Token()
+			from filter in CompleteFilter
+			select new Filter<T>.Not(filter);
+
+		private static Parser<Filter<T>> _GetOperationParser(Parser<object> op, Func<string, object, Filter<T>> apply)
+		{
+			Parser<string> property = Parse.LetterOrDigit.AtLeastOnce().Text();
+
+			return property.Then(prop =>
+			{
+				PropertyInfo? propInfo = typeof(T).GetProperty(prop, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+				if (propInfo == null)
+					throw new ValidationException($"The given filter {property} is invalid.");
+
+				Parser<object> value;
+
+				if (propInfo.PropertyType == typeof(int))
+					value = Parse.Number.Select(x => int.Parse(x) as object);
+				else if (propInfo.PropertyType == typeof(float))
+				{
+					value =
+						from a in Parse.Number
+						from dot in Parse.Char('.')
+						from b in Parse.Number
+						select float.Parse($"{a}.{b}") as object;
+				}
+				else if (propInfo.PropertyType == typeof(string))
+				{
+					value = (
+						from lq in Parse.Char('"').Or(Parse.Char('\''))
+						from str in Parse.AnyChar.Where(x => x is not '"' and not '\'').Many().Text()
+						from rq in Parse.Char('"').Or(Parse.Char('\''))
+						select str
+					).Or(Parse.LetterOrDigit.Many().Text());
+				}
+				else if (propInfo.PropertyType.IsEnum)
+				{
+					value = Parse.LetterOrDigit.Many().Text().Select(x =>
+					{
+						if (Enum.TryParse(propInfo.PropertyType, x, true, out object? value))
+							return value!;
+						throw new ValidationException($"Invalid enum value. Unexpected {x}");
+					});
+				}
+				// TODO: Support arrays
+				else
+					throw new ValidationException("Unfilterable field found");
+
+				return
+					from eq in op
+					from val in value
+					select apply(prop, val);
+			});
+		}
+
+		public static readonly Parser<Filter<T>> Eq = _GetOperationParser(
+			Parse.IgnoreCase("eq").Or(Parse.String("=")).Token(),
+			(property, value) => new Eq(property, value)
+		);
+
+		public static readonly Parser<Filter<T>> Ne = _GetOperationParser(
+			Parse.IgnoreCase("ne").Or(Parse.String("!=")).Token(),
+			(property, value) => new Ne(property, value)
+		);
+
+		public static readonly Parser<Filter<T>> Gt = _GetOperationParser(
+			Parse.IgnoreCase("gt").Or(Parse.String(">")).Token(),
+			(property, value) => new Gt(property, value)
+		);
+
+		public static readonly Parser<Filter<T>> Ge = _GetOperationParser(
+			Parse.IgnoreCase("ge").Or(Parse.String(">=")).Token(),
+			(property, value) => new Ge(property, value)
+		);
+
+		public static readonly Parser<Filter<T>> Lt = _GetOperationParser(
+			Parse.IgnoreCase("lt").Or(Parse.String("<")).Token(),
+			(property, value) => new Lt(property, value)
+		);
+
+		public static readonly Parser<Filter<T>> Le = _GetOperationParser(
+			Parse.IgnoreCase("le").Or(Parse.String("<=")).Token(),
+			(property, value) => new Le(property, value)
+		);
+	}
+
+	public static Filter<T>? From(string? filter)
+	{
+		if (filter == null)
+			return null;
+
+		IResult<Filter<T>> ret = FilterParsers.CompleteFilter.End().TryParse(filter);
+		if (ret.WasSuccessful)
+			return ret.Value;
+		throw new ValidationException($"Could not parse filter argument: {ret.Message}. Not parsed: {filter[ret.Remainder.Position..]}");
 	}
 }
