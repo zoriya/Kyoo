b8e432aa9aad4e796999bf46c4b7addb454e36f8
Zoe Roux <zoe.roux@zoriya.dev>
2023-12-01 23:28:28 +0100

    Fix yoshiki insert bug in input

--- front/packages/primitives/src/input.tsx
+++ front/packages/primitives/src/input.tsx
@@ -19,54 +19,57 @@
  */
 
 import { forwardRef, ReactNode, useState } from "react";
-import { TextInput, TextInputProps, View } from "react-native";
+import { TextInput, TextInputProps, View, ViewStyle } from "react-native";
 import { px, Theme, useYoshiki } from "yoshiki/native";
 import { focusReset, ts } from "./utils";
+import { YoshikiEnhanced } from "./image/base-image";
 
 export const Input = forwardRef<
 	TextInput,
 	{
 		variant?: "small" | "big";
 		right?: ReactNode;
+		containerStyle?: YoshikiEnhanced<ViewStyle>;
 	} & TextInputProps
->(function Input({ placeholderTextColor, variant = "small", right, ...props }, ref) {
-	const { css, theme } = useYoshiki();
+>(function Input(
+	{ placeholderTextColor, variant = "small", right, containerStyle, ...props },
+	ref,
+) {
 	const [focused, setFocused] = useState(false);
+	const { css, theme } = useYoshiki();
 
 	return (
 		<View
-			{...css(
-				[
-					{
-						borderColor: (theme) => theme.accent,
-						borderRadius: ts(1),
-						borderWidth: px(1),
-						borderStyle: "solid",
-						padding: ts(0.5),
-						flexDirection: "row",
-						alignContent: "center",
-						alignItems: "center",
-					},
-					variant === "big" && {
-						borderRadius: ts(4),
-						p: ts(1),
-					},
-					focused && {
-						borderWidth: px(2),
-					},
-				],
-				props,
-			)}
+			{...css([
+				{
+					borderColor: (theme) => theme.accent,
+					borderRadius: ts(1),
+					borderWidth: px(1),
+					borderStyle: "solid",
+					padding: ts(0.5),
+					flexDirection: "row",
+					alignContent: "center",
+					alignItems: "center",
+				},
+				variant === "big" && {
+					borderRadius: ts(4),
+					p: ts(1),
+				},
+				focused && {
+					borderWidth: px(2),
+				},
+				containerStyle,
+			])}
 		>
 			<TextInput
 				ref={ref}
-				placeholderTextColor={placeholderTextColor ?? theme.colors.white}
+				placeholderTextColor={placeholderTextColor ?? theme.paragraph}
 				onFocus={() => setFocused(true)}
 				onBlur={() => setFocused(false)}
 				{...css(
 					{
 						flexGrow: 1,
-						color: (theme: Theme) => theme.colors.white,
+						color: (theme: Theme) => theme.paragraph,
 						borderWidth: 0,
 						...focusReset,
 					},
