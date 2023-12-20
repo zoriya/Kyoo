da85fae3111037f1597fd8cfbddcae60000a4c7d
Zoe Roux <zoe.roux@zoriya.dev>
2023-12-20 03:17:43 +0100

    Implement the select component for mobile

--- front/packages/primitives/src/button.tsx
+++ front/packages/primitives/src/button.tsx
@@ -18,23 +18,28 @@
  * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { ComponentProps } from "react";
+import { ComponentProps, ReactElement, forwardRef } from "react";
 import { Theme, useYoshiki } from "yoshiki/native";
 import { PressableFeedback } from "./links";
 import { P } from "./text";
 import { ts } from "./utils";
+import { View } from "react-native";
 
-export const Button = ({
-	text,
-	...props
-}: { text: string } & ComponentProps<typeof PressableFeedback>) => {
+export const Button = forwardRef<
+	View,
+	{ text: string; icon?: ReactElement } & ComponentProps<typeof PressableFeedback>
+>(function Button({ text, icon, ...props }, ref) {
 	const { css } = useYoshiki("button");
 
 	return (
 		<PressableFeedback
+			ref={ref}
 			{...css(
 				{
 					flexGrow: 0,
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "center",
 					overflow: "hidden",
 					p: ts(0.5),
 					borderRadius: ts(5),
@@ -49,6 +54,7 @@ export const Button = ({
 			)}
 		>
 			<P {...css({ textAlign: "center" }, "text")}>{text}</P>
+			{icon}
 		</PressableFeedback>
 	);
-};
+});
