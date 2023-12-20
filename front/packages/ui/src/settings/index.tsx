b20eb15d951d9ccb973b21dd514bddcf231b34ab
Zoe Roux <zoe.roux@zoriya.dev>
2023-12-20 04:33:31 +0100

    Add download quality setting

--- front/packages/ui/src/settings/index.tsx
+++ front/packages/ui/src/settings/index.tsx
@@ -19,14 +19,24 @@
  */
 
 import { QueryIdentifier, QueryPage, User, UserP, setUserTheme, useUserTheme } from "@kyoo/models";
-import { Container, P, Select, ts } from "@kyoo/primitives";
+import { Container, H1, H2, IconButton, P, Select, tooltip, ts } from "@kyoo/primitives";
 import { DefaultLayout } from "../layout";
 import { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
-import { ScrollView, View } from "react-native";
+import { Platform, ScrollView, ToastAndroid, View } from "react-native";
 import { useYoshiki } from "yoshiki/native";
+import Info from "@material-symbols/svg-400/rounded/info.svg";
 
-const Preference = ({ label, children, ...props }: { label: string; children: ReactNode }) => {
+const Preference = ({
+	label,
+	info,
+	children,
+	...props
+}: {
+	label: string;
+	info?: string;
+	children: ReactNode;
+}) => {
 	const { css } = useYoshiki();
 
 	return (
@@ -42,7 +52,20 @@ const Preference = ({ label, children, ...props }: { label: string; children: Re
 				props,
 			)}
 		>
-			<P>{label}</P>
+			<View {...css({ flexDirection: "row" })}>
+				<P>{label}</P>
+				{info && (
+					<IconButton
+						icon={Info}
+						onPress={
+							Platform.OS === "android"
+								? () => ToastAndroid.show(info, ToastAndroid.LONG)
+								: undefined
+						}
+						{...tooltip(info)}
+					/>
+				)}
+			</View>
 			{children}
 		</View>
 	);
@@ -67,6 +90,21 @@ export const SettingsPage: QueryPage = () => {
 						onValueChange={(value) => setUserTheme(value)}
 						values={["auto", "light", "dark"]}
 						getLabel={(key) => t(`settings.theme.${key}`)}
+					/>
+				</Preference>
+
+				<H2>{t("settings.downloads.title")}</H2>
+				<Preference
+					label={t("settings.downloads.quality.label")}
+					info={t("settings.downloads.quality.info")}
+				>
+					<Select
+						label={t("settings.downloads.quality.label")}
+						value={"original"}
+						// TODO: Implement this setter
+						onValueChange={(value) => {}}
+						values={["original", "8k", "4k", "1440p", "1080p", "720p", "480p", "360p", "240p"]}
+						getLabel={(key) => (key === "original" ? t("player.direct") : key)}
 					/>
 				</Preference>
 			</Container>
