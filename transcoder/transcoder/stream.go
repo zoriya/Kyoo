68304af99e838a74a080ead12efa791840f4f164
Zoe Roux <zoe.roux@zoriya.dev>
2024-01-12 01:17:27 +0100

    Allow transcoder to run concurently

--- transcoder/transcoder/stream.go
+++
@@ -1,9 +0,0 @@
-package transcoder
-
-type TranscodeStream struct {
-	File    FileStream
-	Clients []string
-	// true if the segment at given index is completed/transcoded, false otherwise
-	segments []bool
-	// TODO: add ffmpeg process
-}
