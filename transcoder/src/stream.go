dd9e0b4fc7b6af31cda904e66229c0af96f2b105
Zoe Roux <zoe.roux@zoriya.dev>
2024-01-19 23:49:10 +0100

    Prevent unnecesarry future head creation

--- transcoder/src/stream.go
+++ transcoder/src/stream.go
@@ -222,7 +222,7 @@ func (ts *Stream) GetSegment(segment int32) (string, error) {
 	distance := 0.
 	if !ready {
 		distance = ts.getMinEncoderDistance(segment)
-	} else {
+	} else if !slices.Contains(ts.heads, segment) {
 		// if the current segment is ready, check next segments are ready and if not start a transcode for them
 		for i := segment + 1; i <= min(segment+10, int32(len(ts.segments)-1)); i++ {
 			if slices.Contains(ts.heads, i) {
