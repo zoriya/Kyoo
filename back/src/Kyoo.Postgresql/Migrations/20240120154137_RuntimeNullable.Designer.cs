7a8cc242ae154e2eee2f70f27ceb7a133a2ad47a
Zoe Roux <zoe.roux@zoriya.dev>
2024-01-20 16:43:14 +0100

    Add runtime nullable migration

--- back/src/Kyoo.Postgresql/Migrations/20240120154137_RuntimeNullable.Designer.cs
+++ back/src/Kyoo.Postgresql/Migrations/20240120154137_RuntimeNullable.Designer.cs
@@ -2,17 +2,23 @@
 using System;
 using System.Collections.Generic;
 using Kyoo.Abstractions.Models;
+using Kyoo.Postgresql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
 namespace Kyoo.Postgresql.Migrations
 {
 	[DbContext(typeof(PostgresContext))]
-	partial class PostgresContextModelSnapshot : ModelSnapshot
+	[Migration("20240120154137_RuntimeNullable")]
+	partial class RuntimeNullable
 	{
-		protected override void BuildModel(ModelBuilder modelBuilder)
+		/// <inheritdoc />
+		protected override void BuildTargetModel(ModelBuilder modelBuilder)
 		{
 #pragma warning disable 612, 618
 			modelBuilder
