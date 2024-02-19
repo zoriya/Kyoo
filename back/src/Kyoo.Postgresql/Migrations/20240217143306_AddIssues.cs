1ba03ba9090efac3994211447916d414ff6e1069
Zoe Roux <zoe.roux@zoriya.dev>
2024-02-19 18:07:40 +0100

    Fix play permission migration not behing applied

--- back/src/Kyoo.Postgresql/Migrations/20240217143306_AddIssues.cs
+++ back/src/Kyoo.Postgresql/Migrations/20240217143306_AddIssues.cs
@@ -44,11 +44,6 @@ namespace Kyoo.Postgresql.Migrations
 				principalColumn: "id",
 				onDelete: ReferentialAction.SetNull
 			);
-
-			// language=PostgreSQL
-			migrationBuilder.Sql(
-				"update users set permissions = ARRAY_APPEND(permissions, 'overall.play');"
-			);
 		}
 
 		/// <inheritdoc />
