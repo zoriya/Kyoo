f64cf161ab9adc399495e2c10e31a7a28dcbb91d
Zoe Roux <zoe.roux@zoriya.dev>
2024-04-01 22:21:42 +0200

    Remove all autofac references

--- back/src/Kyoo.Authentication/AuthenticationModule.cs
+++ back/src/Kyoo.Authentication/AuthenticationModule.cs
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Autofac;
 using Kyoo.Abstractions.Controllers;
 using Kyoo.Authentication.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
