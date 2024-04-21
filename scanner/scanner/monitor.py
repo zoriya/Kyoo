ac3b593b8b47d5707089c276e834eda0fed99811
Zoe Roux <zoe.roux@zoriya.dev>
2024-04-21 02:18:10 +0200

    Add ignore permissions errors on watchfile

--- scanner/scanner/monitor.py
+++ scanner/scanner/monitor.py
@@ -7,7 +7,7 @@ logger = getLogger(__name__)
 
 
 async def monitor(path: str, publisher: Publisher):
-	async for changes in awatch(path):
+	async for changes in awatch(path, ignore_permission_denied=True):
 		for event, file in changes:
 			if event == Change.added:
 				await publisher.add(file)
