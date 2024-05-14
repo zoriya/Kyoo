aa89263cdebb2f32ee7fbc378fd3be7aacb123bd
Zoe Roux <zoe.roux@zoriya.dev>
2024-05-14 23:03:03 +0200

    Fix empty ignore pattern

--- scanner/scanner/scanner.py
+++ scanner/scanner/scanner.py
@@ -12,10 +12,13 @@ logger = getLogger(__name__)
 
 def get_ignore_pattern():
 	try:
-		return re.compile(os.environ.get("LIBRARY_IGNORE_PATTERN", ""))
+		pattern = os.environ.get("LIBRARY_IGNORE_PATTERN")
+		if pattern:
+			return re.compile(pattern)
+		return None
 	except Exception as e:
 		logger.error(f"Invalid ignore pattern. Ignoring. Error: {e}")
-		return re.compile("")
+		return None
 
 
 async def scan(
@@ -30,9 +33,10 @@ async def scan(
 	videos = [
 		os.path.join(dir, file) for dir, _, files in os.walk(path) for file in files
 	]
-	to_register = [
-		p for p in videos if p not in registered and not ignore_pattern.match(p)
-	]
+	if ignore_pattern is not None:
+		logger.info(f"Ignoring with pattern {ignore_pattern}")
+		videos = [p for p in videos if not ignore_pattern.match(p)]
+	to_register = [p for p in videos if p not in registered]
 
 	if remove_deleted:
 		deleted = [x for x in registered if x not in videos]
