5f598729563541090e8b043a65bf8e19ed8e6d39
Zoe Roux <zoe.roux@zoriya.dev>
2024-08-08 00:20:25 +0200

    Fix video paths

--- back/src/Kyoo.Core/Views/Content/VideoApi.cs
+++ back/src/Kyoo.Core/Views/Content/VideoApi.cs
@@ -78,18 +78,18 @@ public class VideoApi : Controller
 		await _Proxy($"{path}/master.m3u8");
 	}
 
-	[HttpGet("{path:base64}/{quality}/index.m3u8")]
+	[HttpGet("{path:base64}/{video:int}/{quality}/index.m3u8")]
 	[PartialPermission(Kind.Play)]
-	public async Task GetVideoIndex(string path, string quality)
+	public async Task GetVideoIndex(string path, int video, string quality)
 	{
-		await _Proxy($"{path}/{quality}/index.m3u8");
+		await _Proxy($"{path}/{video}/{quality}/index.m3u8");
 	}
 
-	[HttpGet("{path:base64}/{quality}/{segment}")]
+	[HttpGet("{path:base64}/{video:int}/{quality}/{segment}")]
 	[PartialPermission(Kind.Play)]
-	public async Task GetVideoSegment(string path, string quality, string segment)
+	public async Task GetVideoSegment(string path, int video, string quality, string segment)
 	{
-		await _Proxy($"{path}/{quality}/{segment}");
+		await _Proxy($"{path}/{video}/{quality}/{segment}");
 	}
 
 	[HttpGet("{path:base64}/audio/{audio}/index.m3u8")]
