cb89f224495dfc01a6bfd29ad547325ff25837df
Zoe Roux <zoe.roux@zoriya.dev>
2024-10-24 21:56:16 +0200

    Adding migrations boilerplate

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,16 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+
+if (!process.env.DATABASE_URL) {
+	console.error("Missing `DATABASE_URL` environment variable. Exiting");
+	process.exit(1);
+}
+
+const db = drizzle(process.env.DATABASE_URL);
+
+await migrate(db, { migrationsFolder: "" });
 
 const app = new Elysia()
 	.use(swagger())
