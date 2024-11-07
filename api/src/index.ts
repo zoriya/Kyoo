e7ed36caff31271a7257db8eb198dc8017aff8c5
Zoe Roux <zoe.roux@zoriya.dev>
2024-11-07 11:34:22 +0100

    Add jwt verification on the api

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,10 +3,25 @@ import { swagger } from "@elysiajs/swagger";
 import { db } from "./db";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { movies } from "./controllers/movies";
+import jwt from "@elysiajs/jwt";
 
 await migrate(db, { migrationsFolder: "" });
 
+let secret = process.env.JWT_SECRET;
+if (!secret) {
+	const auth = process.env.AUTH_SERVER ?? "http://auth:4568";
+	const ret = await fetch(`${auth}/info`);
+	const info = await ret.json();
+	secret = info.publicKey;
+}
+
+if (!secret) {
+	console.error("missing jwt secret or auth server. exiting");
+	process.exit(1);
+}
+
 const app = new Elysia()
+	.use(jwt({ secret }))
 	.use(swagger())
 	.get("/", () => "Hello Elysia")
 	.use(movies)
