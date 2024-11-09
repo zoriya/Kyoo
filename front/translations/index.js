eaf220191b335d450c75128b89d021796a1d45fb
Marinho <felipevm97@gmail.com>
2024-11-09 13:21:18 -0300

    chg: feat: add brazillian portuguese to language options

--- front/translations/index.js
+++ front/translations/index.js
@@ -5,6 +5,7 @@ import pl from "./pl";
 import tr from "./tr";
 import uk from "./uk";
 import zh from "./zh";
+import pt_br from "./pt_br";
 
 export default {
 	de: { translation: de },
@@ -14,4 +15,5 @@ export default {
 	tr: { translation: tr },
 	uk: { translation: uk },
 	zh: { translation: zh },
+	pt_br: { translation: pt_br },
 };
