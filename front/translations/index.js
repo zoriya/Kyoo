bda5d710ee6a7c0d7e648b8d39e8bb9523869ba9
Felipe Marinho <felipevm97@gmail.com>
2024-11-09 13:43:50 -0300

    chg: fix: organize imports

--- front/translations/index.js
+++ front/translations/index.js
@@ -2,10 +2,10 @@ import de from "./de";
 import en from "./en";
 import fr from "./fr";
 import pl from "./pl";
+import pt_br from "./pt_br";
 import tr from "./tr";
 import uk from "./uk";
 import zh from "./zh";
-import pt_br from "./pt_br";
 
 export default {
 	de: { translation: de },
