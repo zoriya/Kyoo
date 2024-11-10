cee6993ed080e74c6b6a4feac758564b07cb2c36
Felipe Marinho <felipevm97@gmail.com>
2024-11-10 10:58:17 -0300

    Feat: Add missing languages options (#667)
    * chg: feat: add brazillian portuguese to language options
    
    * chg: fix: organize imports
    
    * chg: feat: add al missing translations
    
    * chg: fix: lint issues
    
    * chg: fix: revert changes, lol

--- front/translations/index.js
+++ front/translations/index.js
@@ -1,7 +1,13 @@
 import de from "./de";
 import en from "./en";
+import es from "./es";
 import fr from "./fr";
+import it from "./it";
+import ko from "./ko";
 import pl from "./pl";
+import pt_br from "./pt_br";
+import ro from "./ro";
+import ru from "./ru";
 import tr from "./tr";
 import uk from "./uk";
 import zh from "./zh";
@@ -9,8 +15,14 @@ import zh from "./zh";
 export default {
 	de: { translation: de },
 	en: { translation: en },
+	es: { translation: es },
 	fr: { translation: fr },
+	it: { translation: it },
+	ko: { translation: ko },
 	pl: { translation: pl },
+	"pt-BR": { translation: pt_br },
+	ro: { translation: ro },
+	ru: { translation: ru },
 	tr: { translation: tr },
 	uk: { translation: uk },
 	zh: { translation: zh },
