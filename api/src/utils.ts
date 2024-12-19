05d5ac5a75e7135c53cbbcb960fb1f80d1cf7bc4
Zoe Roux <zoe.roux@zoriya.dev>
2024-12-19 20:07:18 +0100

    Allow \n in doc comments

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -1,6 +1,6 @@
 // remove indent in multi-line comments
 export const comment = (str: TemplateStringsArray, ...values: any[]) =>
-	str.reduce((acc, str, i) => `${acc}${str}${values[i]}`).replace(/^\s+/gm, "");
+	str.reduce((acc, str, i) => `${acc}${str}${values[i]}`).replace(/^[^\S\n]+/gm, "");
 
 export type RemovePrefix<
 	T extends string,
