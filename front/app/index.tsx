b6b15b0813be373185d67c4155d4785b990e8d62
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-05 17:24:37 +0100

    Delete yarn, setup one

---
+++ front/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function MyApp() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+				minHeight: "100%",
+			}}
+		>
+			<Text>Hello from One</Text>
+		</View>
+	);
+}
