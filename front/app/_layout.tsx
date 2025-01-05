9b89b00b9ffc428bcf7c33cec592ed3007562141
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-05 20:42:41 +0100

    Start providers setup

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,5 +1,25 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Slot } from "one";
+import { Providers } from "~/providers";
 
 export default function Layout() {
-	return <Slot />;
+	return (
+		<>
+			{typeof document !== "undefined" && (
+				<>
+					<title>Kyoo</title>
+					<meta name="description" content="A portable and vast media library solution." />
+					<link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="64x64" href="/icon-64x64.png" />
+					<link rel="icon" type="image/png" sizes="128x128" href="/icon-128x128.png" />
+					<link rel="icon" type="image/png" sizes="256x256" href="/icon-256x256.png" />
+				</>
+			)}
+			<Providers>
+				<Slot />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</Providers>
+		</>
+	);
 }
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,5 +3,9 @@ import type { UserConfig } from "vite";
 import { one } from "one/vite";
 
 export default {
+	ssr: {
+		// needed to fix ssr error of react-query
+		noExternal: true,
+	},
 	plugins: [one()],
 } satisfies UserConfig;
