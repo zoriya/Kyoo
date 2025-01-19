988b705a30b49e1effa65b515f2faf16ccb82ed4
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-19 16:41:02 +0100

    Fix schema utils to make drizzle-kit work

--- api/src/db/schema/shows.ts
+++ api/src/db/schema/shows.ts
@@ -1,4 +1,4 @@
-import { relations, SQL, sql } from "drizzle-orm";
+import { relations, type SQL, sql } from "drizzle-orm";
 import {
 	check,
 	date,
--- api/src/db/schema/utils.ts
+++ api/src/db/schema/utils.ts
@@ -1,26 +1,4 @@
-import {
-	type ColumnsSelection,
-	type SQL,
-	type Subquery,
-	Table,
-	View,
-	ViewBaseConfig,
-	getTableColumns,
-	is,
-	sql,
-} from "drizzle-orm";
-import type { CasingCache } from "drizzle-orm/casing";
-import type { AnyMySqlSelect } from "drizzle-orm/mysql-core";
-import {
-	type AnyPgSelect,
-	customType,
-	jsonb,
-	pgSchema,
-	varchar,
-} from "drizzle-orm/pg-core";
-import type { AnySQLiteSelect } from "drizzle-orm/sqlite-core";
-import type { WithSubquery } from "drizzle-orm/subquery";
-import { db } from "..";
+import { customType, jsonb, pgSchema, varchar } from "drizzle-orm/pg-core";
 
 export const schema = pgSchema("kyoo");
 
@@ -28,61 +6,6 @@ export const language = () => varchar({ length: 255 });
 
 export const image = () =>
 	jsonb().$type<{ id: string; source: string; blurhash: string }>();
-
-// https://github.com/sindresorhus/type-fest/blob/main/source/simplify.d.ts#L58
-type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
-
-// See https://github.com/drizzle-team/drizzle-orm/pull/1789
-type Select = AnyPgSelect | AnyMySqlSelect | AnySQLiteSelect;
-type AnySelect = Simplify<
-	Omit<Select, "where"> & Partial<Pick<Select, "where">>
->;
-export function getColumns<
-	T extends
-		| Table
-		| View
-		| Subquery<string, ColumnsSelection>
-		| WithSubquery<string, ColumnsSelection>
-		| AnySelect,
->(
-	table: T,
-): T extends Table
-	? T["_"]["columns"]
-	: T extends View | Subquery | WithSubquery | AnySelect
-		? T["_"]["selectedFields"]
-		: never {
-	return is(table, Table)
-		? (table as any)[(Table as any).Symbol.Columns]
-		: is(table, View)
-			? (table as any)[ViewBaseConfig].selectedFields
-			: table._.selectedFields;
-}
-
-// See https://github.com/drizzle-team/drizzle-orm/issues/1728
-export function conflictUpdateAllExcept<
-	T extends Table,
-	E extends (keyof T["_"]["columns"])[],
->(table: T, except: E) {
-	const columns = getTableColumns(table);
-	const updateColumns = Object.entries(columns).filter(
-		([col]) => !except.includes(col),
-	);
-
-	return updateColumns.reduce(
-		(acc, [colName, col]) => {
-			// @ts-expect-error: drizzle internal
-			const name = (db.dialect.casing as CasingCache).getColumnCasing(col);
-			acc[colName as keyof typeof acc] = sql.raw(`excluded."${name}"`);
-			return acc;
-		},
-		{} as Omit<Record<keyof T["_"]["columns"], SQL>, E[number]>,
-	);
-}
-
-// drizzle is bugged and doesn't allow js arrays to be used in raw sql.
-export function sqlarr(array: unknown[]) {
-	return `{${array.map((item) => `"${item}"`).join(",")}}`;
-}
 
 // idk why they didn't implement this one
 export const tsvector = customType<{ data: string }>({
