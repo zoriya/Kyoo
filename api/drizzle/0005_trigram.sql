1e2fd919ffc86d9d2e2cc15a7e352070c0c8de23
Zoe Roux <zoe.roux@zoriya.dev>
2025-01-24 19:19:05 +0100

    Add index migration

---
+++ api/drizzle/0005_trigram.sql
@@ -0,0 +1,5 @@
+CREATE INDEX "name_trgm" ON "kyoo"."show_translations" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "tags" ON "kyoo"."show_translations" USING btree ("tags");--> statement-breakpoint
+CREATE INDEX "kind" ON "kyoo"."shows" USING hash ("kind");--> statement-breakpoint
+CREATE INDEX "rating" ON "kyoo"."shows" USING btree ("rating");--> statement-breakpoint
+CREATE INDEX "startAir" ON "kyoo"."shows" USING btree ("start_air");
