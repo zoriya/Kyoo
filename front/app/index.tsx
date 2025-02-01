32ecf253212f748568606ecdb9fbd8fc13f031b1
Zoe Roux <zoe.roux@zoriya.dev>
2025-02-02 00:24:34 +0100

    Start to remove workspace & move theme to root package

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,14 +1,17 @@
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
+import { useYoshiki } from "yoshiki/native";
 
 export default function MyApp() {
+	const { css } = useYoshiki();
+
 	return (
 		<View
-			style={{
+			{...css({
 				flex: 1,
 				justifyContent: "center",
 				alignItems: "center",
 				minHeight: "100%",
-			}}
+			})}
 		>
 			<Text>Hello from One</Text>
 		</View>
