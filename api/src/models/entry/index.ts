a5483b4ca17e3a51c21b5216820f5df88e8ba375
Zoe Roux <zoe.roux@zoriya.dev>
2025-02-19 19:33:26 +0100

    Fix typing & extra handling when fetching entries from db

--- api/src/models/entry/index.ts
+++ api/src/models/entry/index.ts
@@ -1,18 +1,17 @@
 import { t } from "elysia";
-import {
-	Episode,
-	MovieEntry,
-	SeedEpisode,
-	SeedMovieEntry,
-	SeedSpecial,
-	Special,
-} from "../entry";
+import { Extra } from "./extra";
+import { UnknownEntry } from "./unknown-entry";
+import { Episode, SeedEpisode } from "./episode";
+import { MovieEntry, SeedMovieEntry } from "./movie-entry";
+import { SeedSpecial, Special } from "./special";
 
 export const Entry = t.Union([Episode, MovieEntry, Special]);
 export type Entry = Episode | MovieEntry | Special;
 
 export const SeedEntry = t.Union([SeedEpisode, SeedMovieEntry, SeedSpecial]);
 export type SeedEntry = SeedEpisode | SeedMovieEntry | SeedSpecial;
+
+export type EntryKind = Entry["kind"] | Extra["kind"] | UnknownEntry["kind"];
 
 export * from "./episode";
 export * from "./movie-entry";
