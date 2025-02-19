89914f6c95791baeb4af1af93a1e0d2a51baff1f
Zoe Roux <zoe.roux@zoriya.dev>
2025-02-19 22:03:33 +0100

    Format stuff

--- api/src/models/entry/index.ts
+++ api/src/models/entry/index.ts
@@ -1,9 +1,9 @@
 import { t } from "elysia";
-import { Extra } from "./extra";
-import { UnknownEntry } from "./unknown-entry";
 import { Episode, SeedEpisode } from "./episode";
+import type { Extra } from "./extra";
 import { MovieEntry, SeedMovieEntry } from "./movie-entry";
 import { SeedSpecial, Special } from "./special";
+import type { UnknownEntry } from "./unknown-entry";
 
 export const Entry = t.Union([Episode, MovieEntry, Special]);
 export type Entry = Episode | MovieEntry | Special;
