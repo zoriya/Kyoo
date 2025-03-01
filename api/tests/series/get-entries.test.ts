84d9ccb8bb0ee172730d33a401b074209e37b98f
Zoe Roux <zoe.roux@zoriya.dev>
2025-03-01 19:55:41 +0100

    Test extras

--- api/tests/series/get-entries.test.ts
+++ api/tests/series/get-entries.test.ts
@@ -1,5 +1,5 @@
 import { beforeAll, describe, expect, it } from "bun:test";
-import { getEntries } from "tests/helpers";
+import { getEntries, getExtras } from "tests/helpers";
 import { expectStatus } from "tests/utils";
 import { seedSerie } from "~/controllers/seed/series";
 import { madeInAbyss } from "~/models/examples";
@@ -26,5 +26,23 @@ describe("Get entries", () => {
 
 		expectStatus(resp, body).toBe(200);
 		expect(body.items).toBeArrayOfSize(madeInAbyss.entries.length);
+	});
+});
+
+describe("Get extra", () => {
+	it("Invalid slug", async () => {
+		const [resp, body] = await getExtras("sotneuhn", {});
+
+		expectStatus(resp, body).toBe(404);
+		expect(body).toMatchObject({
+			status: 404,
+			message: expect.any(String),
+		});
+	});
+	it("Default sort order", async () => {
+		const [resp, body] = await getExtras(madeInAbyss.slug, {});
+
+		expectStatus(resp, body).toBe(200);
+		expect(body.items).toBeArrayOfSize(madeInAbyss.extras.length);
 	});
 });
