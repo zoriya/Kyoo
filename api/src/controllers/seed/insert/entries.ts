6a301736280f7e028651fff9cdeea2c40449e7bb
Zoe Roux <zoe.roux@zoriya.dev>
2025-03-01 19:21:53 +0100

    Fix poster creation of movie entries

--- api/src/controllers/seed/insert/entries.ts
+++ api/src/controllers/seed/insert/entries.ts
@@ -90,6 +90,7 @@ export const insertEntries = async (
 					language: "extra",
 					name: seed.name,
 					description: null,
+					poster: undefined,
 				};
 			}
 
@@ -98,6 +99,10 @@ export const insertEntries = async (
 				pk: ret[i].pk,
 				language: lang,
 				...tr,
+				poster:
+					seed.kind === "movie"
+						? processOptImage((tr as any).poster)
+						: undefined,
 			}));
 		});
 		await tx
