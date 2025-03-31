8ef4fe5e55362ec40ad1c4baae7f3ed2f54aebf7
Zoe Roux <zoe.roux@zoriya.dev>
2025-03-31 09:41:10 +0200

    Remove db conf handling and read private key from storage

--- auth/config.go
+++ auth/config.go
@@ -27,56 +27,39 @@ var DefaultConfig = Configuration{
 	ExpirationDelay: 30 * 24 * time.Hour,
 }
 
-const (
-	JwtPrivateKey = "jwt_private_key"
-)
-
 func LoadConfiguration(db *dbc.Queries) (*Configuration, error) {
-	ctx := context.Background()
-	confs, err := db.LoadConfig(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	ret := DefaultConfig
-
-	for _, conf := range confs {
-		switch conf.Key {
-		case JwtPrivateKey:
-			block, _ := pem.Decode([]byte(conf.Value))
-			key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-			if err != nil {
-				return nil, err
-			}
-			ret.JwtPrivateKey = key
-			ret.JwtPublicKey = &key.PublicKey
-		}
-	}
 
 	ret.PublicUrl = os.Getenv("PUBLIC_URL")
 	ret.Prefix = os.Getenv("KEIBI_PREFIX")
 
-	if ret.JwtPrivateKey == nil {
+	rsa_pk_path := os.Getenv("RSA_PRIVATE_KEY_PATH")
+	if rsa_pk_path != "" {
+		privateKeyData, err := os.ReadFile(rsa_pk_path)
+		if err != nil {
+			return nil, err
+		}
+
+		block, _ := pem.Decode(privateKeyData)
+		if block == nil || block.Type != "RSA PRIVATE KEY" {
+			return nil, err
+		}
+
+		ret.JwtPrivateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			pkcs8Key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+			if err != nil {
+				return nil, err
+			}
+			ret.JwtPrivateKey = pkcs8Key.(*rsa.PrivateKey)
+		}
+	} else {
+		var err error
 		ret.JwtPrivateKey, err = rsa.GenerateKey(rand.Reader, 4096)
 		if err != nil {
 			return nil, err
 		}
 		ret.JwtPublicKey = &ret.JwtPrivateKey.PublicKey
-
-		pemd := pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PRIVATE KEY",
-				Bytes: x509.MarshalPKCS1PrivateKey(ret.JwtPrivateKey),
-			},
-		)
-
-		_, err := db.SaveConfig(ctx, dbc.SaveConfigParams{
-			Key:   JwtPrivateKey,
-			Value: string(pemd),
-		})
-		if err != nil {
-			return nil, err
-		}
 	}
 
 	return &ret, nil
