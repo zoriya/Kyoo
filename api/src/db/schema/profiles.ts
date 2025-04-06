22754442adb17421b0081462d661027966940d67
Zoe Roux <zoe.roux@zoriya.dev>
2025-04-06 18:24:49 +0200

    Create migration for watchlist/history/profiles

--- api/src/db/schema/profiles.ts
+++ api/src/db/schema/profiles.ts
@@ -1,6 +1,9 @@
-import { integer } from "drizzle-orm/pg-core";
+import { integer, uuid } from "drizzle-orm/pg-core";
 import { schema } from "./utils";
 
+// user info is stored in keibi (the auth service).
+// this table is only there for relations.
 export const profiles = schema.table("profiles", {
 	pk: integer().primaryKey().generatedAlwaysAsIdentity(),
+	id: uuid().notNull().unique(),
 });
