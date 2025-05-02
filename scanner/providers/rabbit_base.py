cf7bc456e8bcf1de17341c2f199d43590ade8435
Fred Heinecke <fred.heinecke@yahoo.com>
2025-05-02 08:06:23 +0000

    Fix Scanner service crash when RabbitMQ queues are predeclared
    Signed-off-by: Fred Heinecke <fred.heinecke@yahoo.com>

--- scanner/providers/rabbit_base.py
+++ scanner/providers/rabbit_base.py
@@ -13,9 +13,22 @@ class RabbitBase:
 			login=os.environ.get("RABBITMQ_DEFAULT_USER", "guest"),
 			password=os.environ.get("RABBITMQ_DEFAULT_PASS", "guest"),
 		)
+
+		# Attempt to declare the queue passively in case it already exists.
+		try:
+			self._channel = await self._con.channel()
+			self._queue = await self._channel.declare_queue(self.QUEUE, passive=True)
+			return self
+		except Exception:
+			# The server will close the channel on error.
+			# Cleanup the reference to it.
+			await self._channel.close()
+
+		# The queue does not exist, so actively declare it.
 		self._channel = await self._con.channel()
 		self._queue = await self._channel.declare_queue(self.QUEUE)
 		return self
 
 	async def __aexit__(self, exc_type, exc_value, exc_tb):
+		await self._channel.close()
 		await self._con.close()
