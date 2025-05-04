4480757df579697eede96c55da7a551cafffea09
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-05 01:39:03 +0200

    Type `Video` in the scanner

---
+++ scanner/scanner/models/extra.py
@@ -0,0 +1,11 @@
+from typing import Literal
+
+type ExtraKind = (
+	Literal["other"] |
+	Literal["trailer"] |
+	Literal["interview"] |
+	Literal["behind-the-scene"] |
+	Literal["deleted-scene"] |
+	Literal["blooper"]
+)
+
