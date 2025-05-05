82a5300ba8b6e2800d51793c55f6a2415b187749
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-05 12:37:29 +0200

    Write identify function

---
+++ scanner/scanner/identify.py
@@ -0,0 +1,47 @@
+from .models.videos import Video, Guess
+from .guess.guess import guessit
+from typing import Literal
+from itertools import zip_longest
+
+
+async def identify(path: str) -> Video:
+	raw = guessit(path, expected_titles=[])
+
+	# guessit should only return one (according to the doc)
+	title: str = raw.get("title", [])[0]
+	kind: Literal["movie"] | Literal["episode"] = raw.get("type", [])[0]
+	version: int = raw.get("version", [])[0]
+	# apparently guessit can return multiples but tbh idk what to do with
+	# multiples part. we'll just ignore them for now
+	part: int = raw.get("part", [])[0]
+
+	years: list[int] = raw.get("year", [])
+	seasons: list[int] = raw.get("season", [])
+	episodes: list[int] = raw.get("episode", [])
+
+	guess = Guess(
+		title=title,
+		kind=kind,
+		extraKind=None,
+		years=years,
+		episodes=[
+			Guess.Episode(season=s, episode=e)
+			for s, e in zip_longest(
+				seasons,
+				episodes,
+				fillvalue=seasons[-1] if len(seasons) < len(episodes) else episodes[-1],
+			)
+		],
+		# TODO: add external ids parsing in guessit
+		external_id={},
+		from_="guessit",
+		raw=raw,
+	)
+
+	return Video(
+		path=path,
+		rendering="",
+		part=part,
+		version=version,
+		guess=guess,
+	)
