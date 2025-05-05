eb70f8f802a109971956fd2dcdb947fe0d240448
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-05 12:37:29 +0200

    Write identify function

--- scanner/scanner/guess/guess.py
+++ scanner/scanner/guess/guess.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 from guessit.api import default_api
 from typing import cast, List, Any
 from rebulk import Rebulk
+from rebulk.match import MatchesDict
 
 try:
 	from . import rules
@@ -20,35 +21,41 @@ rblk = cast(Rebulk, default_api.rebulk)
 rblk.rules(rules)
 
 
-def guessit(name: str, *, xem_titles: List[str] = [], extra_flags: dict[str, Any] = {}):
+def guessit(
+	name: str,
+	*,
+	expected_titles: List[str] = [],
+	extra_flags: dict[str, Any] = {},
+) -> MatchesDict:
 	return default_api.guessit(
 		name,
 		{
 			"episode_prefer_number": True,
 			"excludes": "language",
-			"expected_title": xem_titles,
+			"expected_title": expected_titles,
+			"enforce_list": True
 		}
 		| extra_flags,
 	)
 
 
-# Only used to test localy
+# Only used to test locally
 if __name__ == "__main__":
 	import sys
 	import json
-	from providers.implementations.thexem import TheXemClient
+	# from providers.implementations.thexem import TheXemClient
 	from guessit.jsonutils import GuessitEncoder
 	from aiohttp import ClientSession
 	import asyncio
 
 	async def main():
 		async with ClientSession() as client:
-			xem = TheXemClient(client)
+			# xem = TheXemClient(client)
 
 			advanced = any(x == "-a" for x in sys.argv)
 			ret = guessit(
 				sys.argv[1],
-				xem_titles=await xem.get_expected_titles(),
+				expected_titles=[],
 				extra_flags={"advanced": advanced},
 			)
 			print(json.dumps(ret, cls=GuessitEncoder, indent=4))
