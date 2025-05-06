fff0c0ea91f17b75ffa3c9cee97c6b4abd46a3f9
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-06 02:27:31 +0200

    Proper `rendering` computation (god that was a pain)

--- scanner/scanner/guess/guess.py
+++ scanner/scanner/guess/guess.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python3
-
-if __name__ == "__main__":
-	import sys
-	from pathlib import Path
-
-	sys.path.append(str(Path(f"{__file__}/../../..").resolve()))
+from typing import Any, List, cast
 
 from guessit.api import default_api
-from typing import cast, List, Any
 from rebulk import Rebulk
 from rebulk.match import Match
 
@@ -27,8 +20,7 @@ def guessit(
 	expected_titles: List[str] = [],
 	extra_flags: dict[str, Any] = {},
 ) -> dict[str, list[Match]]:
-	rendering = []
-	ret = default_api.guessit(
+	return default_api.guessit(
 		name,
 		{
 			"episode_prefer_number": True,
@@ -36,34 +28,6 @@ def guessit(
 			"expected_title": expected_titles,
 			"enforce_list": True,
 			"advanced": True,
-			"rendering": rendering,
 		}
 		| extra_flags,
 	)
-	print(rendering)
-	return ret
-
-
-# Only used to test locally
-if __name__ == "__main__":
-	import sys
-	import json
-
-	# from providers.implementations.thexem import TheXemClient
-	from guessit.jsonutils import GuessitEncoder
-	from aiohttp import ClientSession
-	import asyncio
-
-	async def main():
-		async with ClientSession() as client:
-			# xem = TheXemClient(client)
-
-			advanced = any(x == "-a" for x in sys.argv)
-			ret = guessit(
-				sys.argv[1],
-				expected_titles=[],
-				extra_flags={"advanced": advanced},
-			)
-			print(json.dumps(ret, cls=GuessitEncoder, indent=4))
-
-	asyncio.run(main())
