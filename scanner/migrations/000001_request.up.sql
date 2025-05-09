1da585dd4581b99f12fd5b54107712aeab836483
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-10 00:12:50 +0200

    Implement request processor (listen for requests)

--- scanner/migrations/000001_request.up.sql
+++ scanner/migrations/000001_request.up.sql
@@ -1,6 +1,15 @@
 begin;
 
-create type scanner.request_kind as enum('episode', 'movie');
+create type scanner.request_kind as enum(
+	'episode',
+	'movie'
+);
+
+create type scanner.request_status as enum(
+	'pending',
+	'running',
+	'failed'
+);
 
 create table scanner.requests(
 	pk serial primary key,
@@ -8,11 +17,13 @@ create table scanner.requests(
 	kind scanner.request_kind not null,
 	title text not null,
 	year integer,
-	external_id jsonb not null default '{}'::jsonb,
-
-	created_at timestamptz not null default now()::timestamptz,
-
-	constraint unique_kty (kind, title, year)
+	external_id jsonb not null default '{}' ::jsonb,
+	status scanner.request_status not null,
+	started_at created_at timestamptz,
+	created_at created_at timestamptz not null default now() ::timestamptz,
+	constraint unique_kty(kind, title, year),
+	constraint unique_eid(external_id)
 );
 
 commit;
+
