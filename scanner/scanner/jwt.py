6427aafc4d6e09dc1f5644df51c13d034286c757
Zoe Roux <zoe.roux@zoriya.dev>
2025-05-11 03:37:34 +0200

    Fix jwks validation in scanner

--- scanner/scanner/jwt.py
+++ scanner/scanner/jwt.py
@@ -1,10 +1,13 @@
 import os
+from logging import getLogger
 from typing import Annotated
 
 import jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, SecurityScopes
 from jwt import PyJWKClient
+
+logger = getLogger(__name__)
 
 jwks_client = PyJWKClient(
 	os.environ.get("JWKS_URL", "http://auth:4568/.well-known/jwks.json")
@@ -21,6 +24,7 @@ def validate_bearer(
 		payload = jwt.decode(
 			token.credentials,
 			jwks_client.get_signing_key_from_jwt(token.credentials).key,
+			algorithms=["RS256"],
 			issuer=os.environ.get("JWT_ISSUER"),
 		)
 		for scope in perms.scopes:
@@ -34,6 +38,7 @@ def validate_bearer(
 				)
 		return payload
 	except Exception as e:
+		logger.error("Failed to parse token", exc_info=e)
 		raise HTTPException(
 			status_code=403,
 			detail="Could not validate credentials",
