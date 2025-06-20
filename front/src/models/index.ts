9498dea3fd3c1d59f8a29dae9150578f62fabaf5
Zoe Roux <zoe.roux@zoriya.dev>
2025-06-20 17:30:10 +0200

    Add show type

--- front/src/models/index.ts
+++ front/src/models/index.ts
@@ -4,6 +4,7 @@ export * from "./kyoo-error";
 export * from "./movie";
 export * from "./serie";
 export * from "./collection";
+export * from "./show";
 export * from "./entry";
 export * from "./studio";
 export * from "./video";
---
+++ front/src/models/show.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+import { Collection } from "./collection";
+import { Movie } from "./movie";
+import { Serie } from "./serie";
+
+export const Show = z.union([
+	Serie.and(z.object({ kind: z.literal("serie") })),
+	Movie.and(z.object({ kind: z.literal("movie") })),
+	Collection.and(z.object({ kind: z.literal("collection") })),
+]);
