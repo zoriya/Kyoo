787bfc1151ac1eda36a0fac174ec27a394526b91
Zoe Roux <zoe.roux@zoriya.dev>
2025-07-19 21:12:37 +0200

    Type video in the front

--- front/src/models/video.ts
+++ front/src/models/video.ts
@@ -1,4 +1,46 @@
 import { z } from "zod/v4";
+import { Entry } from "./entry";
+import { Extra } from "./extra";
+import { zdate } from "./utils/utils";
+
+export const Video = z.object({
+	id: z.string(),
+	path: z.string(),
+	rendering: z.string(),
+	part: z.int().min(0).nullable(),
+	version: z.int().min(0).default(1),
+	guess: z.object({
+		title: z.string(),
+		kind: z.enum(["episode", "movie", "extra"]).nullable().optional(),
+		extraKind: Extra.shape.kind.optional().nullable(),
+		years: z.array(z.int()).default([]),
+		episodes: z
+			.array(
+				z.object({
+					season: z.int().nullable(),
+					episode: z.int(),
+				}),
+			)
+			.default([]),
+		externalId: z.record(z.string(), z.string()).default({}),
+
+		// Name of the tool that made the guess
+		from: z.string(),
+		get history() {
+			return z.array(Video.shape.guess.omit({ history: true })).default([]);
+		},
+	}),
+	createdAt: zdate(),
+	updatedAt: zdate(),
+});
+
+export const FullVideo = Video.extend({
+	slugs: z.array(z.string()),
+	entries: z.array(Entry),
+	previous: z.object({ video: z.string(), entry: Entry }).nullable().optional(),
+	next: z.object({ video: z.string(), entry: Entry }).nullable().optional(),
+});
+export type FullVideo = z.infer<typeof FullVideo>;
 
 export const EmbeddedVideo = z.object({
 	id: z.string(),
