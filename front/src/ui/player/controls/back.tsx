e01c67b1ef7b29180efdf604be667c1223631e44
Zoe Roux <zoe.roux@zoriya.dev>
2025-07-26 23:09:24 +0200

    Fix player controls style

--- front/src/ui/player/controls/back.tsx
+++ front/src/ui/player/controls/back.tsx
@@ -11,7 +11,7 @@ import {
 	tooltip,
 } from "~/primitives";
 
-export const Back = ({ name, ...props }: { name: string } & ViewProps) => {
+export const Back = ({ name, ...props }: { name?: string } & ViewProps) => {
 	const { css } = useYoshiki();
 	const { t } = useTranslation();
 	const router = useRouter();
@@ -35,49 +35,19 @@ export const Back = ({ name, ...props }: { name: string } & ViewProps) => {
 				onPress={router.back}
 				{...tooltip(t("player.back"))}
 			/>
-			<H1
-				{...css({
-					alignSelf: "center",
-					fontSize: rem(1.5),
-					marginLeft: rem(1),
-				})}
-			>
-				{name}
-			</H1>
-		</View>
-	);
-};
-
-Back.Loader = (props: ViewProps) => {
-	const { css } = useYoshiki();
-	const { t } = useTranslation();
-	const router = useRouter();
-
-	return (
-		<View
-			{...css(
-				{
-					position: "absolute",
-					top: 0,
-					left: 0,
-					right: 0,
-					bg: (theme) => theme.darkOverlay,
-					display: "flex",
-					flexDirection: "row",
-					alignItems: "center",
-					padding: percent(0.33),
-					color: "white",
-				},
-				props,
+			{name ? (
+				<H1
+					{...css({
+						alignSelf: "center",
+						fontSize: rem(1.5),
+						marginLeft: rem(1),
+					})}
+				>
+					{name}
+				</H1>
+			) : (
+				<Skeleton {...css({ width: rem(5) })} />
 			)}
-		>
-			<IconButton
-				icon={ArrowBack}
-				as={PressableFeedback}
-				onPress={router.back}
-				{...tooltip(t("player.back"))}
-			/>
-			<Skeleton {...css({ width: rem(5) })} />
 		</View>
 	);
 };
