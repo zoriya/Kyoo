a30a4065e2d9880329d3bd9f9bc175ea4ee1f80e
Antoine Labarussias <antoinelabarussias@gmail.com>
2025-11-02 09:18:50 +0100

    fix(front): fix auth token to load images on mobile

--- front/src/primitives/image-background.tsx
+++ front/src/primitives/image-background.tsx
@@ -36,7 +36,7 @@ export const ImageBackground = ({
 				headers:
 					authToken && Platform.OS !== "web"
 						? {
-								Authorization: authToken,
+								Authorization: `Bearer ${authToken}`,
 							}
 						: undefined,
 			}}
