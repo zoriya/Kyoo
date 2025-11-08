951ae955edd96b600b3a4ac5c2f9d5816d2a01d1
Zoe Roux <zoe.roux@zoriya.dev>
2025-11-08 17:45:09 +0100

    Use a postinstall script to generate translation list

--- front/src/providers/translations.compile.ts
+++ front/src/providers/translations.compile.ts
@@ -1,22 +1,71 @@
-// this file is run at compile time thanks to a vite plugin
+// this file is auto-generated via a postinstall script.
 
-// import { readFile, readdir } from "node:fs/promises";
-// import type { Resource } from "i18next";
-
-// const translationDir = new URL("../../public/translations/", import.meta.url);
-// const langs = await readdir(translationDir);
-
-// export const resources: Resource = Object.fromEntries(
-// 	await Promise.all(
-// 		langs.map(async (x) => [
-// 			x.replace(".json", ""),
-// 			{ translation: JSON.parse(await readFile(new URL(x, translationDir), "utf8")) },
-// 		]),
-// 	),
-// );
-
+import am from "../../public/translations/am.json";
+import ar from "../../public/translations/ar.json";
+import de from "../../public/translations/de.json";
 import en from "../../public/translations/en.json";
+import es from "../../public/translations/es.json";
+import fr from "../../public/translations/fr.json";
+import gl from "../../public/translations/gl.json";
+import is from "../../public/translations/is.json";
+import it from "../../public/translations/it.json";
+import ko from "../../public/translations/ko.json";
+import ml from "../../public/translations/ml.json";
+import nl from "../../public/translations/nl.json";
+import pl from "../../public/translations/pl.json";
+import pt from "../../public/translations/pt.json";
+import pt_BR from "../../public/translations/pt-BR.json";
+import ro from "../../public/translations/ro.json";
+import ru from "../../public/translations/ru.json";
+import ta from "../../public/translations/ta.json";
+import tr from "../../public/translations/tr.json";
+import uk from "../../public/translations/uk.json";
+import zh from "../../public/translations/zh.json";
 
-export const resources = { en };
+export const resources = {
+	am: { translation: am },
+	ar: { translation: ar },
+	de: { translation: de },
+	en: { translation: en },
+	es: { translation: es },
+	fr: { translation: fr },
+	gl: { translation: gl },
+	is: { translation: is },
+	it: { translation: it },
+	ko: { translation: ko },
+	ml: { translation: ml },
+	nl: { translation: nl },
+	pl: { translation: pl },
+	pt: { translation: pt },
+	"pt-BR": { translation: pt_BR },
+	ro: { translation: ro },
+	ru: { translation: ru },
+	ta: { translation: ta },
+	tr: { translation: tr },
+	uk: { translation: uk },
+	zh: { translation: zh },
+};
 
-export const supportedLanguages = Object.keys(resources);
+export const supportedLanguages = [
+	"am",
+	"ar",
+	"de",
+	"en",
+	"es",
+	"fr",
+	"gl",
+	"is",
+	"it",
+	"ko",
+	"ml",
+	"nl",
+	"pl",
+	"pt",
+	"pt-BR",
+	"ro",
+	"ru",
+	"ta",
+	"tr",
+	"uk",
+	"zh",
+];
