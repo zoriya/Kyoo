6e3fe885b6fd29be227c9b8c716cef38892321a9
Zoe Roux <zoe.roux@zoriya.dev>
2025-12-21 16:24:22 +0100

    fixup! Rework home header

--- front/src/ui/home/header.tsx
+++ front/src/ui/home/header.tsx
@@ -123,64 +123,75 @@ export const Header = ({
 };
 
 Header.Loader = () => {
-	const { css, theme } = useYoshiki();
 	const { t } = useTranslation();
 
 	return (
-		<View
-			{...css({
-				width: { md: percent(70) },
-				margin: ts(2),
-			})}
-		>
-			<LinearGradient
-				start={{ x: 0, y: 0.25 }}
-				end={{ x: 0, y: 1 }}
-				colors={["transparent", theme.darkOverlay]}
-				{...(css({
-					width: percent(100),
-					height: {
-						xs: vh(40),
-						sm: min(vh(60), px(750)),
-						md: min(vh(60), px(680)),
-						lg: vh(65),
-					},
-					minHeight: {
-						xs: px(350),
-						sm: px(300),
-						md: px(400),
-						lg: px(600),
-					},
-					position: "absolute",
-					top: 0,
-					left: 0,
-					right: 0,
-				}) as any)}
-			/>
-			<Skeleton {...css({ width: rem(8), height: rem(2.5) })} />
-			<View {...css({ flexDirection: "row", alignItems: "center" })}>
-				<IconFab
-					icon={PlayArrow}
-					aria-label={t("show.play")}
-					{...tooltip(t("show.play"))}
-					{...css({ marginRight: ts(1) })}
-				/>
-				<IconButton
-					icon={Info}
-					aria-label={t("home.info")}
-					{...tooltip(t("home.info"))}
-					{...css({ marginRight: ts(2) })}
-				/>
-				<Skeleton
+		<ContrastArea mode="dark">
+			{({ css, theme }) => (
+				<View
 					{...css({
-						width: rem(25),
-						height: rem(2),
-						display: { xs: "none", sm: "flex" },
+						flexDirection: "column-reverse",
+						width: percent(100),
+						height: {
+							xs: vh(40),
+							sm: min(vh(60), px(750)),
+							md: min(vh(60), px(680)),
+							lg: vh(65),
+						},
+						minHeight: {
+							xs: px(350),
+							sm: px(300),
+							md: px(400),
+							lg: px(600),
+						},
 					})}
-				/>
-			</View>
-			<Skeleton lines={4} {...css({ marginTop: ts(1) })} />
-		</View>
+				>
+					<LinearGradient
+						start={{ x: 0, y: 0.25 }}
+						end={{ x: 0, y: 1 }}
+						colors={["transparent", theme.darkOverlay]}
+						{...(css({
+							position: "absolute",
+							top: 0,
+							bottom: 0,
+							left: 0,
+							right: 0,
+						}) as any)}
+					/>
+					<View {...css({ margin: ts(2) })}>
+						<Skeleton {...css({ width: rem(8), height: rem(2.5) })} />
+						<View {...css({ flexDirection: "row", alignItems: "center" })}>
+							<IconFab
+								icon={PlayArrow}
+								aria-label={t("show.play")}
+								{...tooltip(t("show.play"))}
+								{...css({ marginRight: ts(1) })}
+							/>
+							<IconButton
+								icon={Info}
+								aria-label={t("home.info")}
+								{...tooltip(t("home.info"))}
+								{...css({ marginRight: ts(2) })}
+							/>
+							<Skeleton
+								{...css({
+									width: rem(25),
+									height: rem(2),
+									display: { xs: "none", sm: "flex" },
+								})}
+							/>
+						</View>
+						<Skeleton
+							lines={4}
+							{...css({
+								display: { xs: "none", md: "flex" },
+								marginTop: ts(1),
+							})}
+						/>
+					</View>
+				</View>
+			)}
+		</ContrastArea>
 	);
 };
 
