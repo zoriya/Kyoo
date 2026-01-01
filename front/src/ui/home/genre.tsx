8f71099e7eb98db4dd117ecd3fd24b54a39b1508
Zoe Roux <zoe.roux@zoriya.dev>
2026-01-01 16:41:59 +0100

    Rework genres, news & recommended of home page

--- front/src/ui/home/genre.tsx
+++ front/src/ui/home/genre.tsx
@@ -1,31 +1,12 @@
-/*
- * Kyoo - A portable and vast media library solution.
- * Copyright (c) Kyoo.
- *
- * See AUTHORS.md and LICENSE file in the project root for full license information.
- *
- * Kyoo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * Kyoo is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Kyoo. If not, see <https://www.gnu.org/licenses/>.
- */
-
 import { useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { useYoshiki } from "yoshiki/native";
 import { ItemGrid, itemMap } from "~/components/items";
-import type { Genre, Show } from "~/models";
+import { type Genre, Show } from "~/models";
 import { H3, ts } from "~/primitives";
 import { InfiniteFetch, type QueryIdentifier } from "~/query";
+import { EmptyView } from "../errors";
 
 export const Header = ({ title }: { title: string }) => {
 	const { css } = useYoshiki();
@@ -56,7 +37,11 @@ export const GenreGrid = ({ genre }: { genre: Genre }) => {
 				query={GenreGrid.query(genre)}
 				layout={{ ...ItemGrid.layout, layout: "horizontal" }}
 				placeholderCount={2}
-				empty={displayEmpty.current ? t("home.none") : undefined}
+				Empty={
+					displayEmpty.current ? (
+						<EmptyView message={t("home.none")} />
+					) : undefined
+				}
 				Render={({ item }) => <ItemGrid {...itemMap(item)} />}
 				Loader={ItemGrid.Loader}
 			/>
@@ -69,7 +54,6 @@ GenreGrid.query = (genre: Genre): QueryIdentifier<Show> => ({
 	infinite: true,
 	path: ["api", "shows"],
 	params: {
-		fields: ["watchStatus"],
 		filter: `genres has ${genre}`,
 		sort: "random",
 		// Limit the initial numbers of items
